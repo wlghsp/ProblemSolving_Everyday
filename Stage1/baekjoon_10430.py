674731197fe99861441c38a92a3f0d4df2ba0e21
wlghsp <wlghsp@gmail.com>
2020-08-26 15:23:33 +0900

    first_commit

---
+++ Stage1/baekjoon_10430.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+
+a, b, c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+print((a + b) % c)
+print(((a % c) + (b % c)) % c)
+print((a * b) % c)
+print(((a % c) * (b % c)) % c)
---
+++ Stage1/baekjoon_10869.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+print(a % b)
