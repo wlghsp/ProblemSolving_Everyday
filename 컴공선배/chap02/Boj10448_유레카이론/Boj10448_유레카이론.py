81f6f32e91a45763daff6ad5d94c525354116aeb
CHOIJIHO <wlghsp@gmail.com>
2022-05-01 14:11:24 +0900

    Boj10448_유레카이론
    - 완전탐색
    - 미리 값을 구해 놓고 세개 더해서 K가 나오는 것 찾기 위해
    - 3중 포문 구현

---
+++ 컴공선배/chap02/Boj10448_유레카이론/Boj10448_유레카이론.py
@@ -0,0 +1,19 @@
+
+
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+T = [n * (n+1) // 2 for n in range(46)]
+
+
+def is_possible(K):
+    for i in range(1, 46):
+        for j in range(i, 46):
+            for k in range(j, 46):
+                if T[i] + T[j] + T[k] == K:
+                    return 1
+
+    return 0
+
+for _ in range(int(input())):
+    print(is_possible(int(input())))
