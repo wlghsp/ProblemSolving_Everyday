01089ecd787b4e3a6f61fff123489b44973db0dc
CHOIJIHO <wlghsp@gmail.com>
2022-05-01 14:10:28 +0900

    Boj3040_백설공주와일곱난쟁이
    - combinations, 9개에서 2개빼서 100나오는 풀이

---
+++ 컴공선배/chap02/Boj3040_백설공주와일곱난쟁이/Boj3040_백설공주와일곱난쟁이_2.py
@@ -0,0 +1,21 @@
+
+
+import sys
+
+input = lambda : sys.stdin.readline().rstrip()
+
+h = [int(input()) for _ in range(9)]
+
+tot = sum(h)
+
+def solve():
+    for i in range(8):
+        for j in range(i+1, 9):
+            if tot - h[i] - h[j] == 100:
+                for k in h:
+                    if k != h[i] and k != h[j]:
+                        print(k)
+
+                break
+
+solve()
