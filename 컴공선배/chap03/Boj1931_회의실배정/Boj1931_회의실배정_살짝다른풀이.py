4a002a7518ca39fa57de0d73996d8ff0ed242db8
CHOIJIHO <wlghsp@gmail.com>
2022-05-05 17:48:34 +0900

    Boj1931_회의실배정
    - 종료시간을 기준으로 정렬하고 시작시간 정렬을 두번째 정렬조건으로 설정하거나
    - (종료시간,시작시간) 이렇게 배열에 넣어두면 파이썬 정렬은 알아서 정렬해준다.
    - 그리고 시작시간이 종료시간보다 크거나 같을때만 다른 회의가 가능한 경우 체크하고 이 회의의 종료시간을 다시 t에 담고 다음 회의들의 시작시간을 체크한다.

---
+++ 컴공선배/chap03/Boj1931_회의실배정/Boj1931_회의실배정_살짝다른풀이.py
@@ -0,0 +1,20 @@
+import sys
+
+input = lambda : sys.stdin.readline().rstrip()
+
+
+meetings = [tuple(map(int, input().split()))for _ in range(int(input()))]
+
+meetings.sort(key=lambda x: (x[1], x[0]))
+
+
+t = 0
+
+ans = 0
+
+for start, end in meetings:
+    if t <= start:
+        ans += 1
+        t = end
+
+print(ans)
