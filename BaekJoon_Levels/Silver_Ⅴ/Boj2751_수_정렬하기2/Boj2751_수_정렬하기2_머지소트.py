2910a913770518d26e194d674dda5826b4d86bc2
CHOIJIHO <wlghsp@gmail.com>
2022-05-13 08:36:04 +0900

    Boj2751_수_정렬하기2
    * 풀이
    - 정렬방법을 공부할 수 있는 문제이다.
    - 합병정렬과 계수정렬로 풀이
    - 머지소트는 아직은 풀이만 적었다. 이해는 못했다.

---
+++ BaekJoon_Levels/Silver_Ⅴ/Boj2751_수_정렬하기2/Boj2751_수_정렬하기2_머지소트.py
@@ -0,0 +1,38 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+input = lambda : sys.stdin.readline().rstrip()
+
+N = int(input())
+arr = []
+for i in range(N):
+    arr.append(int(input()))
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    L = merge_sort(arr[:mid]) # 분할
+    R = merge_sort(arr[mid:]) # 분할
+    mer = []
+
+    i = 0
+    j = 0
+    while i < len(L) and j < len(R): # 정렬 및 결합
+        if (L[i] > R[j]):
+            mer.append(R[j])
+            j += 1
+        else:
+            mer.append(L[i])
+            i += 1
+
+    if i != len(L):
+        mer += L[i:]
+    if j != len(R):
+        mer += R[j:]
+    return mer
+
+mer = merge_sort(arr)
+for i in mer:
+    print(i)
