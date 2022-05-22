3dc8e0aabe6c84e75152a00f48f625d7a66d0131
CHOIJIHO <wlghsp@gmail.com>
2022-05-22 10:41:16 +0900

    Boj10845_큐.py
    * 풀이
    - 큐2 문제랑 다른 점이 뭔지 모르겠다

---
+++ BaekJoon_Levels/Silver_Ⅳ/Boj10845_큐/Boj10845_큐.py
@@ -0,0 +1,21 @@
+
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+from collections import deque
+
+N = int(input())
+dq = deque()
+for _ in range(N):
+    command = input().split()
+    if command[0] == 'push':
+        dq.append(command[1])
+    elif command[0] == 'pop':
+        print(dq.popleft() if dq else -1)
+    elif command[0] == 'size':
+        print(len(dq))
+    elif command[0] == 'empty':
+        print(0 if dq else 1)
+    elif command[0] == 'front':
+        print(dq[0] if dq else -1)
+    elif command[0] == 'back':
+        print(dq[-1] if dq else -1)
