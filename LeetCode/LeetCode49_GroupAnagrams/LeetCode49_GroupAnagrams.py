ae2bbb8756e1a887348f2d207e2238df98c3679c
CHOIJIHO <wlghsp@gmail.com>
2022-05-31 08:44:44 +0900

    출근 전 반영

---
+++ LeetCode/LeetCode49_GroupAnagrams/LeetCode49_GroupAnagrams.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+def groupAnagrams(strs: List[str]) -> List[List[str]]:
+    pass
+
+
+
+strs = ["eat","tea","tan","ate","nat","bat"]
+print(groupAnagrams(strs))
--- LeetCode/finished/LeetCode617_MergeTwoBinaryTrees/Solution.py
+++ LeetCode/finished/LeetCode617_MergeTwoBinaryTrees/Solution.py
@@ -8,12 +8,13 @@ class TreeNode:
         self.right = right
 
 
-def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-    if root1 and root2:
-        node = TreeNode(root1.val + root2.val)
-        node.left = self.mergeTrees(root1.left, root2.left)
-        node.right = self.mergeTrees(root1.right, root2.right)
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root1 and root2:
+            node = TreeNode(root1.val + root2.val)
+            node.left = self.mergeTrees(root1.left, root2.left)
+            node.right = self.mergeTrees(root1.right, root2.right)
 
-        return node
-    else:
-        return root1 or root2
+            return node
+        else:
+            return root1 or root2
