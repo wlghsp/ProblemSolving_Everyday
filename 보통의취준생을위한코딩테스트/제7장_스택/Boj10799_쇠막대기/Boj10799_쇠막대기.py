48fe9897c84720538e3065341a5f83b15ae1f104
CHOIJIHO <wlghsp@gmail.com>
2022-06-03 22:42:42 +0900

    Boj10799_쇠막대기.py - 리팩토링

--- 보통의취준생을위한코딩테스트/제7장_스택/Boj10799_쇠막대기/Boj10799_쇠막대기.py
+++ 보통의취준생을위한코딩테스트/제7장_스택/Boj10799_쇠막대기/Boj10799_쇠막대기.py
@@ -10,11 +10,10 @@ for i in range(len(galho)):
     if galho[i] == '(':
         stk.append(galho[i])
     else:
+        stk.pop()
         if galho[i-1] == '(':
-            stk.pop()
             answer += len(stk)
         else:
-            stk.pop()
             answer += 1
 
 print(answer)
