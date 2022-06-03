f355dc03bb91b930c27b75ea767c2f26731493cb
wlghsp <wlghsp@gmail.com>
2022-06-03 17:56:25 +0900

    Boj1439_뒤집기.py
    * 풀이
    - 0으로 뒤집는 경우와 1로 뒤집는 경우 둘을 세고 둘의 최솟값을 출력한다

---
+++ BaekJoon_Levels/Silver_Ⅴ/Boj1439_뒤집기/Boj1439_뒤집기.py
@@ -0,0 +1,26 @@
+
+
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+from collections import Counter
+
+S = input()
+
+
+count0 = 0
+count1 = 0
+
+if S[0] == '1':
+    count0 += 1
+else:
+    count1 += 1
+
+
+for i in range(len(S) - 1):
+    if S[i] != S[i+1]:
+        if S[i + 1] == '1':
+            count0 += 1
+        else:
+            count1 += 1
+
+print(min(count0, count1))
