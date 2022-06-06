bd8542787fae37b7ccc67508a8011a9ca7aa866b
CHOIJIHO <wlghsp@gmail.com>
2022-06-07 08:28:47 +0900

    주석해제

--- LeetCode/finished/LeetCode88_MergeSortedArray.py
+++ LeetCode/finished/LeetCode88_MergeSortedArray.py
@@ -1,24 +1,24 @@
 from typing import List
 
 # 1. 내 풀이
-# def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-#     j = 0
-#     for i in range(m, m + len(nums2)):
-#         nums1[i] = nums2[j]
-#         j += 1
-# 
-#     nums1 = sorted(nums1)
-#     print(nums1)
+def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    j = 0
+    for i in range(m, m + len(nums2)):
+        nums1[i] = nums2[j]
+        j += 1
+
+    nums1 = sorted(nums1)
+    print(nums1)
 
 # 1. 정렬
-# def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-#      for i, v in enumerate(nums2):
-#         nums1[m+i] = v
-#      nums1[:] = sorted(nums1)
-#      print(nums1)
+def merge1(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+     for i, v in enumerate(nums2):
+        nums1[m+i] = v
+     nums1[:] = sorted(nums1)
+     print(nums1)
 
 # 2. 비교 및 삽입
-def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+def merge2(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
     i = m - 1
     j = n - 1
     k = m + n - 1
@@ -45,4 +45,4 @@ m = 3
 nums2 = [2,5,6]
 n = 3
 
-merge(nums1, m, nums2, n) # [1,2,2,3,5,6]
+merge2(nums1, m, nums2, n) # [1,2,2,3,5,6]
