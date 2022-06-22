96c39b0b9f2d0b027b95348fe9777b359e2cd94e
CHOIJIHO <wlghsp@gmail.com>
2022-06-23 08:11:36 +0900

    풀이 예정

---
+++ LeetCode/LeetCode168_ExcelSheetColumnTitle/LeetCode168_ExcelSheetColumnTitle.py
@@ -0,0 +1,19 @@
+
+
+
+
+class Solution:
+    def convertToTitle(self, num: int) -> str:
+        answer = ""
+
+        
+
+
+
+
+
+columnNumber = 701
+sol = Solution()
+print(sol.convertToTitle(columnNumber)) # "ZY"
+print(sol.convertToTitle(28)) # "AB"
+print(sol.convertToTitle(1)) # "A"
