6fb5e58705577176f29e3c77f2729afcd728b54c
CHOIJIHO <wlghsp@gmail.com>
2022-06-29 19:28:09 +0900

    기존에 풀었던 파일 삭제

--- BaekJoon_Levels/Bronze_Ⅱ/다시풀기/baekjoon_10275.py
+++
@@ -1,7 +0,0 @@
-import sys
-input = sys.stdin.readline
-
-# 9223372036854775807 9223372036854775808
-
-a, b = map(int, input().split())
-print(a+b)
--- BaekJoon_Levels/Bronze_Ⅱ/다시풀기/baekjoon_11720.py
+++
@@ -1,7 +0,0 @@
-a = int(input())
-b = input()
-result = 0
-for i in list(b):
-    result += int(i)
-
-print(result)
