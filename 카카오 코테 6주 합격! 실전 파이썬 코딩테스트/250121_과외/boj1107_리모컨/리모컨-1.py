08b899b255bdfa819f7490118cc780acab9ac5cf
JIHO CHOI <wlghsp@gmail.com>
2025-01-23 08:46:13 +0900

    리모컨 풀이 23일 출근전 풀이
    - 결과: 틀렸습니다.

--- 카카오 코테 6주 합격! 실전 파이썬 코딩테스트/250121_과외/boj1107_리모컨/리모컨-1.py
+++ 카카오 코테 6주 합격! 실전 파이썬 코딩테스트/250121_과외/boj1107_리모컨/리모컨-1.py
@@ -1,49 +1,38 @@
-from bisect import bisect_left
-from itertools import product
 
 N = input()# 이동하려고 하는 채널
 length = len(N)
 N = int(N)
 M = int(input()) # 고장난 버튼의 개수
 remote_buttons = [i for i in range(10)]
-first_digits = [i for i in range(1, 10)]
-if M > 0:
+broken = [False] * 10
+cur = 100 # 현재 채널 100번
+
+if M == 10:
+    print(abs(cur - N))
+    exit()
+elif M == 0:
+    print(length)
+    exit()
+elif M > 0:
     broken_buttons = list(map(int, input().split())) # 고장난 버튼 번호
     for button in broken_buttons:
         remote_buttons.remove(button)
-        if button != 0:
-            first_digits.remove(button)
 
-cur = 100 # 현재 채널 100번
+min_cnt = abs(cur - N)
+def recur(picked, depth, length):
+    global min_cnt
+    if depth == length:
+        number = int(''.join(map(str, picked)))
+        min_cnt = min(min_cnt, len(picked) + abs(N - number))
+        return
 
-def generate_comb_numbers(first_digits, remote_buttons, length):
-    comb_numbers = []
-    if len(remote_buttons) == 1:
-        for i in range(1, length + 2):
-            comb_numbers.append(int(str(remote_buttons[0]) * i))
-        return comb_numbers
-    else:
-        for first in first_digits:
-            for comb in product(remote_buttons, repeat= length - 1):
-                num = int(str(first) + ''.join(map(str, comb)))
-                comb_numbers.append(num)
-    return comb_numbers
+    for i in remote_buttons:
+        picked.append(i)
+        recur(picked, depth + 1, length)
+        picked.pop()
 
 
-def find_closest(sorted_numbers, target):
-    index = bisect_left(sorted_numbers, target)
+for i in range(1, 8):
+    recur([], 0, i)
 
-    left_value = sorted_numbers[index - 1] if index > 0 else float('inf')
-    right_value = sorted_numbers[index] if index < len(sorted_numbers) else float('inf')
-
-    # 가장 가까운 값 반환
-    if abs(target - left_value) <= abs(target - right_value):
-        return left_value
-    else:
-        return right_value
-
-comb_numbers = generate_comb_numbers(first_digits, remote_buttons, length)
-sorted_numbers = sorted(comb_numbers)
-closest_num = find_closest(sorted_numbers, N)
-
-print(min(len(str(closest_num)) + abs(closest_num - N), abs(cur - N)))
+print(min_cnt)
