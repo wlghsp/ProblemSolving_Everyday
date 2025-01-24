562dc5007657b6ea9b6760531705cd30dafcb433
JIHO CHOI <jh.choi2@okestro.com>
2025-01-24 09:13:58 +0900

    백준 1107 리모컨 1회 풀이 완료
    - 고장난 버튼 갯수에 따른 처리가 필요
    - 중복순열로 숫자를 생성하여 최소 횟수 매번 비교
    - 10의 7승 * 7의 최대연산으로 2초 시간제한(2 * 10의 7승) 가능성이 있으나 통과함

--- 카카오 코테 6주 합격! 실전 파이썬 코딩테스트/250121_과외/boj1107_리모컨/리모컨-1.py
+++ 카카오 코테 6주 합격! 실전 파이썬 코딩테스트/250121_과외/boj1107_리모컨/리모컨-1.py
@@ -1,38 +1,41 @@
 
-N = input()# 이동하려고 하는 채널
+N = input()# 이동 하려고 하는 채널
 length = len(N)
 N = int(N)
 M = int(input()) # 고장난 버튼의 개수
 remote_buttons = [i for i in range(10)]
-broken = [False] * 10
 cur = 100 # 현재 채널 100번
 
-if M == 10:
+if M == 10: # 전부 고장난 경우 +, - 로 이동
     print(abs(cur - N))
-    exit()
-elif M == 0:
-    print(length)
-    exit()
-elif M > 0:
-    broken_buttons = list(map(int, input().split())) # 고장난 버튼 번호
-    for button in broken_buttons:
+elif M == 0: # 고장난 버튼이 없는 겨우
+    # 번호를 눌러 가는 경우와 +, -로 이동 하는 경우 횟수 비교 필요
+    print(min(length, abs(cur - N)))
+else:
+    broken_buttons = list(map(int, input().split())) # 고장난 버튼 번호 입력
+
+    for button in broken_buttons: # 고장난 버튼 번호 삭제
         remote_buttons.remove(button)
 
-min_cnt = abs(cur - N)
-def recur(picked, depth, length):
-    global min_cnt
-    if depth == length:
-        number = int(''.join(map(str, picked)))
-        min_cnt = min(min_cnt, len(picked) + abs(N - number))
-        return
+    min_cnt = abs(cur - N) # 최소 횟수 100 번에서 시작 하는 경우로 초기화
 
-    for i in remote_buttons:
-        picked.append(i)
-        recur(picked, depth + 1, length)
-        picked.pop()
+    # 중복 순열 생성
+    def recur(picked, depth, length):
+        global min_cnt
+
+        if depth == length:
+            number = int(''.join(map(str, picked)))
+            min_cnt = min(min_cnt, length + abs(N - number))
+            return
+
+        for i in remote_buttons:
+            picked.append(i)
+            recur(picked, depth + 1, length)
+            picked.pop()
 
 
-for i in range(1, 8):
-    recur([], 0, i)
+    # 찾는 숫자 보다 큰 경우를 찾기 위해 찾는 숫자 길이 + 1 까지 생성
+    for i in range(1, length + 2):
+        recur([], 0, i)
 
-print(min_cnt)
+    print(min_cnt)
