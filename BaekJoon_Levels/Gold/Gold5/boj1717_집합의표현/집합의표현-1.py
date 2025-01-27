8b0d5490acfeb132e83baf3e8cc674b5a690277f
JIHO CHOI <jh.choi2@okestro.com>
2025-01-27 23:32:01 +0900

    백준 1717 집합의 표현 학습 완료

--- BaekJoon_Levels/Gold/Gold5/boj1717_집합의표현/집합의표현-1.py
+++ BaekJoon_Levels/Gold/Gold5/boj1717_집합의표현/집합의표현-1.py
@@ -1,8 +1,28 @@
-# n + 1 개의 집합 {0},{1},{2}..{n}
-# 1.합집합 연산
-# 2.두 원소가 같은 집합에 포함되어 있는지를 확인하는 연산 수행
-# 집합을 표현하는 프로그램 작성
-# n, m : m은 연산의 개수
-# 합집합 0 a b
-# 같은집합 여부: 1 a b
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+input = lambda : sys.stdin.readline().rstrip()
+
 n, m = map(int, input().split())
+parent = [i for i in range(n + 1)]
+
+# 찾기 연산 (같은 집합에 속하는지 확인하기 위한 함수)
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def union(a, b):
+    pa = find(a)
+    pb = find(b)
+    parent[pa] = pb
+
+for _ in range(m):
+    q, a, b = map(int, input().split())
+    if q == 0: # union
+        union(a, b)
+    elif q == 1: # find
+        if find(a) == find(b):
+            print("YES")
+        else:
+            print("NO")
