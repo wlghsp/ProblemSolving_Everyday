66c4799815267422b0f4add85cda546ec098c71d
JIHO CHOI <jh.choi2@okestro.com>
2025-02-06 13:32:35 +0900

    프로그래머스 정수 삼각형 1회 풀이 완료

--- 카카오 코테 6주 합격! 실전 파이썬 코딩테스트/250204_과제/프로그래머스_정수삼각형/정수삼각형-1.py
+++ 카카오 코테 6주 합격! 실전 파이썬 코딩테스트/250204_과제/프로그래머스_정수삼각형/정수삼각형-1.py
@@ -1,7 +1,21 @@
-def solution(triangle):
-    answer = 0
-    return answer
+def solution(t):
+    dp = []
+    for i in range(1, len(t) + 1):
+        dp.append([0] * i)
+
+    for i in range(len(t)):
+        for j in range(len(t[i])):
+            if j - 1 >= 0 and j + 1 < len(t[i]):
+                dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + t[i][j]
+            elif j - 1 < 0:
+                dp[i][j] = dp[i - 1][j] + t[i][j]
+            elif j + 1 >= len(t[i]):
+                dp[i][j] = dp[i - 1][j - 1] + t[i][j]
+
+    return max(max(row) for row in dp)
 
 
 # 30
+# dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + t[i][j] (j - 1>= 0 and j + 1 < len(t[i]))
+
 print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
