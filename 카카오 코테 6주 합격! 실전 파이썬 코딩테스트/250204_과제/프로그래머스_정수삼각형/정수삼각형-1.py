ac634db8578fa2eaed4d642bf9584c9a4b1d41a2
JIHO CHOI <jh.choi2@okestro.com>
2025-02-06 13:42:49 +0900

    프로그래머스 정수 삼각형 코드 개선

--- 카카오 코테 6주 합격! 실전 파이썬 코딩테스트/250204_과제/프로그래머스_정수삼각형/정수삼각형-1.py
+++ 카카오 코테 6주 합격! 실전 파이썬 코딩테스트/250204_과제/프로그래머스_정수삼각형/정수삼각형-1.py
@@ -2,15 +2,16 @@ def solution(t):
     dp = []
     for i in range(1, len(t) + 1):
         dp.append([0] * i)
+    dp[0][0] = t[0][0] # 첫 번째 값 초기화
 
     for i in range(len(t)):
         for j in range(len(t[i])):
-            if j - 1 >= 0 and j + 1 < len(t[i]):
-                dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + t[i][j]
-            elif j - 1 < 0:
+            if j == 0: # 왼쪽 끝
                 dp[i][j] = dp[i - 1][j] + t[i][j]
-            elif j + 1 >= len(t[i]):
+            elif j == len(t[i]) - 1: # 오른쪽 끝
                 dp[i][j] = dp[i - 1][j - 1] + t[i][j]
+            else:
+                dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + t[i][j]
 
     return max(max(row) for row in dp)
 
