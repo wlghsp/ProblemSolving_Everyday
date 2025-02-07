fafdb7788535937f653591f7bca36fa8dcb12b7c
JIHO CHOI <jh.choi2@okestro.com>
2025-02-07 15:30:51 +0900

    가장 많이 받은 선물 1회 풀이 완료

---
+++ 프로그래머스/가장많이받은선물/가장많이받은선물-2.py
@@ -0,0 +1,34 @@
+def solution(friends, gifts):
+    N = len(friends)
+    I = {name: idx for name, idx in zip(friends, range(N))}
+    give = [0] * N
+    receive = [0] * N
+    record = [[0] * N for _ in range(N)]
+
+    for g in gifts:
+        a, b = g.split()
+        give[I[a]] += 1
+        receive[I[b]] += 1
+        record[I[a]][I[b]] += 1
+
+    scores = [0] * N
+    for x in range(N):
+        scores[x] = give[x] - receive[x]
+
+    gifts = [0] * N
+    for x in range(N):
+        for i in range(N):
+            if x == i: continue
+            # case1 선물을 더 준 경우
+            case1 = (record[x][i] > record[i][x])
+            # case2 선물을 준 횟수는 같은데 선물지수가 더 높은 경우
+            case2 = (record[x][i] == record[i][x] and scores[x] > scores[i])
+            gifts[x] += (case1 or case2)
+
+    return max(gifts)
+
+print(solution(
+["muzi", "ryan", "frodo", "neo"],
+["muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi",
+    "ryan muzi", "frodo muzi", "frodo ryan", "neo muzi"]
+)) # 2
