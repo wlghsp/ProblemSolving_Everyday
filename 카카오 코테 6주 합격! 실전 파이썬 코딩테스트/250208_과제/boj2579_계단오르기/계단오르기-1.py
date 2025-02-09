bda2988753cf0cf3eeabc2d7d2d41428b08fd921
JIHO CHOI <wlghsp@gmail.com>
2025-02-09 15:29:32 +0900

    계단 오르기 1회 풀이 완료

---
+++ 카카오 코테 6주 합격! 실전 파이썬 코딩테스트/250208_과제/boj2579_계단오르기/계단오르기-1.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+scores = [0] * 301
+for i in range(1, N + 1):
+    scores[i] = int(input())
+
+dp = [0] * 301
+dp[1] = scores[1]
+dp[2] = scores[1] + scores[2]
+dp[3] = max(scores[1] + scores[3], scores[2] + scores[3])
+
+for i in range(4, 301):
+    dp[i] = max(dp[i - 2], dp[i - 3] + scores[i - 1]) + + scores[i]
+
+print(dp[N])
