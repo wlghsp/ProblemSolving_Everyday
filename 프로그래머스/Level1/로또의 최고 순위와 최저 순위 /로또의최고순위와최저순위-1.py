a2c7913768f2134c65d5f9ac2974a7fdfc7a4ac9
JIHO CHOI <jh.choi2@okestro.com>
2025-02-12 10:59:18 +0900

    프로그래머스 로또의 최고순위와 최저순위 1회 풀이 완료

--- 프로그래머스/Level1/로또의 최고 순위와 최저 순위 /로또의최고순위와최저순위-1.py
+++ 프로그래머스/Level1/로또의 최고 순위와 최저 순위 /로또의최고순위와최저순위-1.py
@@ -1,5 +1,12 @@
 def solution(lottos, win_nums):
-    answer = []
-    return answer
+    ranks = {cnt: rank for cnt, rank in zip(range(6, 1, -1), range(1, 6))}
+    ranks[0] = 6
+    ranks[1] = 6
+    min_cnt = len(set(lottos) & set(win_nums))
+    zero_cnt = lottos.count(0)
+    max_cnt = min(zero_cnt, 6 - min_cnt) + min_cnt
+    return [ranks[max_cnt], ranks[min_cnt]]
 
-print(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19])) # [3,5]
+# print(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19])) # [3,5]
+print(solution([0, 0, 0, 0, 0, 0], [38, 19, 20, 40, 15, 25])) # 	[1, 6]
+print(solution(	[45, 4, 35, 20, 3, 9], [20, 9, 3, 45, 4, 35])) # 	[1, 1]
