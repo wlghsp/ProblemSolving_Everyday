9e7d535c1034374a19186d594786e9cff4931d87
JIHO CHOI <jh.choi2@okestro.com>
2025-02-14 16:13:54 +0900

    백준 동전 1 1회 학습 완료

---
+++ BaekJoon_Levels/Gold/Gold4/boj2293_동전1/동전1-1.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+N, K = map(int, input().split())
+coins = [int(input()) for _ in range(N)]
+dp = [0] * (K + 1)
+dp[0] = 1
+
+for coin in coins:
+    for j in range(coin, K + 1):
+        dp[j] += dp[j - coin]
+
+print(dp[K])
