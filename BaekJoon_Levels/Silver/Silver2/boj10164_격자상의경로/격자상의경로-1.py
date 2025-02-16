355cf0f14d790404d1afd16c97deac57ba7bebc1
JIHO CHOI <jh.choi2@okestro.com>
2025-02-16 18:33:05 +0900

    백준 10164 격자상의 경로 1회 풀이 완료

--- BaekJoon_Levels/Silver/Silver2/boj10164_격자상의경로/격자상의경로-1.py
+++ BaekJoon_Levels/Silver/Silver2/boj10164_격자상의경로/격자상의경로-1.py
@@ -1,11 +1,16 @@
-
-"""
-오른쪽, 아래로만 이동 가능
-
-"""
 N, M, K = map(int, input().split())
+
 if K == 0:
-    pass
+    dp = [[0] * M for _ in range(N)]
+    for i in range(N):  # 위에서 오는 경우
+        dp[i][0] = 1
+    for i in range(M):  # 왼쪽에서 오는 경우
+        dp[0][i] = 1
+    for i in range(1, N):
+        for j in range(1, M):
+            # 위에서 내려 오거나, 왼쪽에서 오거나
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    print(dp[N - 1][M - 1])
 else:
     board = [[0] * M for _ in range(N)]
     num = 1
@@ -17,22 +22,23 @@ else:
             board[i][j] = num
             num += 1
     dp1 = [[0] * (m1 + 1) for _ in range(n1 + 1)]
-    dp2 = [[0] * M for _ in range(N)]
+    for i in range(n1 + 1): # 위에서 오는 경우
+        dp1[i][0] = 1
+    for i in range(m1 + 1): # 왼쪽에서 오는 경우
+        dp1[0][i] = 1
+    for i in range(1, n1 + 1):
+        for j in range(1, m1 + 1):
+            # 위에서 내려 오거나, 왼쪽에서 오거나
+            dp1[i][j] = dp1[i - 1][j] + dp1[i][j - 1]
 
-    def dfs(x, y, n, m, dp):
+    dp = [[0] * M for _ in range(N)]
+    for i in range(n1, N):  # 위에서 오는 경우
+        dp[i][m1] = 1
+    for i in range(m1, M):  # 왼쪽에서 오는 경우
+        dp[n1][i] = 1
+    for i in range(n1 + 1, N):
+        for j in range(m1 + 1, M):
+            # 위에서 내려 오거나, 왼쪽에서 오거나
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
 
-        for dx, dy in [(0, 1), (1, 0)]:
-            nx, ny = x + dx, y + dy
-            if nx < 0 or nx >= n or ny < 0 or ny >= m: continue
-
-            dp[nx][ny] = dp[x][y] + 1
-
-            dfs(nx, ny, n, m, dp)
-
-    dfs(0, 0, n1 + 1, m1 + 1, dp1)
-    print(dp1)
-
-
-
-
-
+    print(dp1[n1][m1] * dp[N - 1][M - 1])
