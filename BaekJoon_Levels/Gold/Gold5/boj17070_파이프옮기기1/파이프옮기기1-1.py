8514fa95c200a35e2859ca25e69783f1ee844f7b
JIHO CHOI <jh.choi2@okestro.com>
2025-02-17 18:05:46 +0900

    백준 17070 파이프 옮기기 1 dfs-dp 풀이

--- BaekJoon_Levels/Gold/Gold5/boj17070_파이프옮기기1/파이프옮기기1-1.py
+++ BaekJoon_Levels/Gold/Gold5/boj17070_파이프옮기기1/파이프옮기기1-1.py
@@ -1,33 +1,51 @@
-"""
-파이프 최소 위치 (0, 0), (0, 1) = 이 위치는 항상 0 이다.
-
-1 = 벽, 0 = 빈칸
-
-파이프 밀 수 있는 방향 3 가지 , 오른쪽 , 오른쪽 대각선 45도 아래, 아래
-대각선, 좌, 아래
-파이프의 상태 파악 필요
-이동 방법
-가로 - 2가지, 세로 - 2가지, 대각선 - 3가지
-
-
-"""
 import sys
 
 input = lambda : sys.stdin.readline().rstrip()
 
 N = int(input())
 house = [list(map(int, input().split())) for _ in range(N)]
-move_counts = [[0] * N for _ in range(N)]
-pipe_loc = ((0, 0), (0, 1))
-dp = [[0] * N for _ in range(N)]
+dp = [[[-1] * 3 for _ in range(N)] for _ in range(N)]
 
-def dfs(loc):
-    pass
+def three_point_check(x, y):
+    for dx, dy in [(0, 1), (1, 0), (1, 1)]:
+        nx, ny = x + dx, y + dy
+        if nx < 0 or nx >= N or ny < 0 or ny >= N: return False
+        if house[nx][ny] == 1: return False
+    return True
 
-print(dp[N][N])
+def can_move(x, y):
+    return x < N and y < N and house[x][y] == 0
 
+def move_diagonal(direction, x, y):
+    if three_point_check(x, y):  # 대각선
+        dp[x][y][direction] += dfs(x + 1, y + 1, 2)
 
+def move_down(direction, x, y):
+    if can_move(x + 1, y):  # 아래
+        dp[x][y][direction] += dfs(x + 1, y, 1)
 
+def move_right(direction, x, y):
+    if can_move(x, y + 1):  # 오른쪽
+        dp[x][y][direction] += dfs(x, y + 1, 0)
 
+def dfs(x, y, direction):
+    if x == N - 1 and y == N - 1:
+        return 1
 
+    if dp[x][y][direction] != -1:
+        return dp[x][y][direction]
 
+    dp[x][y][direction] = 0
+
+    if direction == 0:
+        move_right(direction, x, y)
+    elif direction == 1:
+        move_down(direction, x, y)
+    elif direction == 2:
+        move_right(direction, x, y)
+        move_down(direction, x, y)
+
+    move_diagonal(direction, x, y)
+    return dp[x][y][direction]
+
+print(dfs(0, 1, 0))
