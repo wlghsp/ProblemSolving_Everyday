9bd865dc233adf93a2b1bdebf8db323ea3f8d9c5
JIHO CHOI <wlghsp@gmail.com>
2025-02-19 23:01:47 +0900

    백준 20546 기적의 매매법 코드 개선

--- BaekJoon_Levels/Silver/Silver5/boj20546_기적의매매법/기적의매매법-1.py
+++ BaekJoon_Levels/Silver/Silver5/boj20546_기적의매매법/기적의매매법-1.py
@@ -4,31 +4,23 @@ prices = list(map(int, input().split()))
 j_cash = cash
 j_stock = 0
 for p in prices:
-    if j_cash <= 0:
-        break
-
     if j_cash >= p:
         j_stock += j_cash // p
         j_cash = j_cash % p
 
-jun = j_cash + j_stock * prices[len(prices) - 1]
-
-
-# 2. seongmin
 s_cash = cash
 s_stock = 0
-for i in range(len(prices) - 3):
-    if prices[i] < prices[i + 1] < prices[i + 2]:
-        if i + 3 < len(prices) and s_stock > 0:
-            s_cash += prices[i + 3] * s_stock
-            s_stock = 0
-    elif prices[i] > prices[i + 1] > prices[i + 2]:
-        if i + 3 < len(prices) and s_cash >= prices[i + 3]:
-            buy_cnt = s_cash // prices[i + 3]
-            s_stock += buy_cnt
-            s_cash -= buy_cnt * prices[i + 3]
-
-seong = s_cash + s_stock * prices[len(prices) - 1]
+for i in range(3, len(prices)):
+    if prices[i - 3] < prices[i - 2] < prices[i] and s_stock > 0:
+        s_cash += prices[i + 3] * s_stock
+        s_stock = 0
+    elif prices[i - 3] > prices[i - 2] > prices[i - 1] and s_cash >= prices[i]:
+        buy_cnt = s_cash // prices[i]
+        s_stock += buy_cnt
+        s_cash -= buy_cnt * prices[i]
+last_price = prices[len(prices) - 1]
+jun = j_cash + j_stock * last_price
+seong = s_cash + s_stock * last_price
 
 if jun > seong:
     print("BNP")
