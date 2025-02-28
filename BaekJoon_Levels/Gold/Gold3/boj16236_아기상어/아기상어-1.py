23b85f9182fd402597adfb3f5dcce95f28c45ccb
JIHO CHOI <jh.choi2@okestro.com>
2025-02-28 21:01:51 +0900

    백준 16236 아기상어 풀이 거의 다했음

--- BaekJoon_Levels/Gold/Gold3/boj16236_아기상어/아기상어-1.py
+++ BaekJoon_Levels/Gold/Gold3/boj16236_아기상어/아기상어-1.py
@@ -1,28 +1,7 @@
-"""
-N x N 크기의 공간에 물고기 M마리와 아기상어 1마리
-한 칸에는 물고기가 최대 1마리 존재
-
-fisrt baby_shark_size = 2
-1초에 상하좌우 이동
-
-- if baby_shark_size < fish_size: continue
-- baby_shark_size >=  fish_size:
-    if baby_shark_size > fish_size:
-        baby_shark can eat
--   else:
-        baby_shark just can pass by
-1. 더 이상 먹을 물고기 없으면 종료
-2. 1마리라면 그 물고기 먹으러 간다
-3. 1마리 초과라면 가장 가까운 물고기를 먹으러 감
-    - 거리는 지나야하는 칸의 개수의 최솟값
-    - 거리가 같다면, x asc, y asc
-아기상어 크기와 같은 수의 물고기 먹으면 크기 증가
-if ate_cnt == baby_shark_size:
-    baby_shark_size += 1
-"""
-import sys
+import heapq
 from collections import deque
 
+INF = 10 ** 9
 N = int(input())
 space = [list(map(int, input().split())) for _ in range(N)]
 cur_x, cur_y = 0, 0
@@ -33,47 +12,79 @@ for i in range(N):
             if space[i][j] == 9:
                 cur_x, cur_y = i, j
             else:
-                fishes.append((i, j))
+                fishes.append((0, i, j))
 
 time = 0
 if len(fishes) == 0:
-    print(time)
+    print(0)
+elif len(fishes) == 1:
+    _, x, y = fishes.popleft()
+    print((abs(cur_x - x) + abs(cur_y - y)))
 else:
     baby_shark_size = 2
-    # 거리 구하기
+
+    # 거리 구하고 먹을 수 있는 물고기 찾기
     def bfs():
-        dist = [[0] * N for _ in range(N)]
+        if len(fishes) == 1:
+            _, x, y = fishes.popleft()
+            return (abs(cur_x - x) + abs(cur_y - y)), x, y
+
+        dist = [[INF] * N for _ in range(N)]
+        dist[cur_x][cur_y] = 0
         q = deque([(cur_x, cur_y)])
 
         while q:
             px, py = q.popleft()
-
             for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                 nx, ny = px + dx, py + dy
                 if nx < 0 or nx >= N or ny < 0 or ny >= N: continue
-                if dist[nx][ny] != 0: continue
+                if dist[nx][ny] != INF: continue
                 if baby_shark_size < space[nx][ny]: continue
 
                 dist[nx][ny] = dist[px][py] + 1
                 q.append((nx, ny))
-        min_dist = sys.maxsize
-        min_x, min_y = 0, 0
-        while fishes:
-            if min_dist > dist[nx][ny]:
-                min_dist = dist[nx][ny]
-                min_x, min_y = nx, ny
-        return min_x, min_y
+
+        heap = []
+        len_f = len(fishes)
+        for i in range(len_f):
+            _, x, y = fishes.popleft()
+            heap.append((dist[x][y], x, y))
+
+        heapq.heapify(heap)
+        target_x, target_y = 0, 0
+        min_dist = INF
+        len_heap = len(heap)
+        found = False
+        for i in range(len_heap):
+            d, x, y = heapq.heappop(heap)
+            if space[x][y] < baby_shark_size and min_dist > d:
+                min_dist, target_x, target_y = d, x, y
+                found = True
+            fishes.append((d, x, y))
+
+        len_fishes = len(fishes)
+        for i in range(len_fishes):
+            d, x, y = fishes.popleft()
+            if d == min_dist and target_x == x and target_y == y: continue
+            fishes.append((d, x, y))
+
+        return min_dist if found else -1, target_x, target_y
+
 
     ate_cnt = 0
-    while fishes:
-        if len(fishes) == 1:
-            cur_x, cur_y = fishes[0], fishes[1]
-        elif len(fishes) > 1:
-            cur_x, cur_y = bfs()
-
+    space[cur_x][cur_y] = 0
+    while True:
+        if len(fishes) == 0:
+            break
+        dist, cur_x, cur_y = bfs()
+        if dist == -1:
+            break
+        space[cur_x][cur_y] = 0
+        ate_cnt += 1
+        time += dist
 
         if ate_cnt == baby_shark_size:
             baby_shark_size += 1
-
+            ate_cnt = 0
 
     print(time)
