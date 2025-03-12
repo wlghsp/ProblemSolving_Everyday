6fa71f64e549ac84a9c2bbb3bccfc9b3d576fa8c
JIHO CHOI <wlghsp@gmail.com>
2025-03-12 23:18:05 +0900

    백준 17144 미세먼지 안녕 풀이 완료

--- BaekJoon_Levels/Gold/Gold4/boj17144_미세먼지안녕/미세먼지안녕-1.py
+++ BaekJoon_Levels/Gold/Gold4/boj17144_미세먼지안녕/미세먼지안녕-1.py
@@ -1,54 +1,71 @@
-"""
--1은 공기청정기 1번 열이며 두 행 차지 위아래 차지
+import copy
+from collections import deque
 
-1. 미세먼지 확산
-  모든 칸에서 동시에 일어남
-  네 방향 확산, 공기청정기나 칸이 없으면 확산 X
-  확산되는 양은 먼지/5 이고 소수점 버림.
-  남은 미세먼지 양은 먼지 - [먼지/5] * 확산된 방향 갯수
-
-2. 공기청정기 작동
-  공기청정기 바람 나옴
-  위쪽 반시계방향 순환, 아래쪽 시계방향 순환
-  바람이 불면 미세먼지가 바람의 방향대로 모두 한칸 씩 이동
-  공기청정기로 들어간 미세먼지는 모두 정화
-"""
 R, C, T = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(R)]
-upper_x = 0
+upper_x, bottom_x = 0, 0
+
 for i in range(R):
     if grid[i][0] == -1 and grid[i + 1][0] == -1:
-        upper_x = i
-
-def get_dusk():
-    result = []
+        upper_x, bottom_x = i, i + 1
+def get_dusk_locs():
+    result = deque()
     for i in range(R):
         for j in range(C):
-            if grid[i][j] != -1 and grid[i][j] != 0:
-                result.append((i, j))
+            if grid[i][j] >= 5:
+                result.append((i, j, int(grid[i][j] / 5)))
     return result
 
-def spread_dusk(dusk):
-    for x, y in dusk:
+def spread_dusk(dusk_locs):
+    while dusk_locs:
+        x, y, spread_amount = dusk_locs.popleft()
         cnt = 0
         for dx, dy in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
             nx, ny = x + dx, y + dy
             if not (0 <= nx < R and 0 <= ny < C): continue
-            if grid[nx][ny] == -1: continue
+            if grid[nx][ny] < 0: continue
 
-            grid[nx][ny] += grid[x][y] // 5
+            grid[nx][ny] += spread_amount
             cnt += 1
-
-        grid[x][y] -= grid[x][y] // 5 * cnt
-
+        grid[x][y] -= spread_amount * cnt
+        if grid[x][y] < 0:
+            grid[x][y] = 0
 
 def operate_purifier():
-    pass
+    # 1. 위쪽 공기청정기 가동
+    grid[upper_x - 1][0] = 0
+    for i in range(upper_x - 2, -1, -1):
+        grid[i + 1][0] = grid[i][0]
+        grid[i][0] = 0
+    for i in range(1, C):
+        grid[0][i - 1] = grid[0][i]
+        grid[0][i] = 0
+    for i in range(1, upper_x + 1):
+        grid[i - 1][C - 1] = grid[i][C - 1]
+        grid[i][C - 1] = 0
+    for i in range(C - 2, 0, -1):
+        grid[upper_x][i + 1] = grid[upper_x][i]
+        grid[upper_x][i] = 0
+
+    # 2. 아래쪽 공기청정기 가동
+    grid[bottom_x + 1][0] = 0
+    for i in range(bottom_x + 2, R):
+        grid[i - 1][0] = grid[i][0]
+        grid[i][0] = 0
+    for i in range(1, C):
+        grid[R - 1][i - 1] = grid[R - 1][i]
+        grid[R - 1][i] = 0
+    for i in range(R - 2, bottom_x - 1, -1):
+        grid[i + 1][C - 1] = grid[i][C - 1]
+        grid[i][C - 1] = 0
+    for i in range(C - 2, 0, -1):
+        grid[bottom_x][i + 1] = grid[bottom_x][i]
+        grid[bottom_x][i] = 0
 
 for _ in range(T):
     # 1. 미세먼지 확산
-    dusk_locs = get_dusk()
-    spread_dusk(dusk_locs)
+    locs = get_dusk_locs()
+    spread_dusk(locs)
 
     # 2. 공기청정기 작동
     operate_purifier()
