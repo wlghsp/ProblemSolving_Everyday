d1cf10032180c2367e3b916ccb75a5d419586e87
JIHO CHOI <wlghsp@gmail.com>
2025-03-14 20:39:07 +0900

    백준 20056 마법사 상어와 파이어볼 코드 개선
    - 딕셔너리의 keys() 대신 items() 사용하여 풀이
    - 질량의 합 처리 시 한줄로 변경

--- BaekJoon_Levels/Gold/Gold4/boj20056_마법사상어와파이어볼/마법사상어와파이어볼-1.py
+++ BaekJoon_Levels/Gold/Gold4/boj20056_마법사상어와파이어볼/마법사상어와파이어볼-1.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import deque, defaultdict
 
 dir = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
 odd_even_dir = [[0, 2, 4, 6], [1, 3, 5, 7]]
@@ -6,27 +6,27 @@ odd_even_dir = [[0, 2, 4, 6], [1, 3, 5, 7]]
 N, M, K = map(int, input().split())
 grid = [[0] * N for _ in range(N)]
 fireball_info = [list(map(int, input().split())) for _ in range(M)]
-fireball_dict = {(r, c): deque() for r in range(N) for c in range(N)}
+fireball_dict = defaultdict(deque)
+
+# 초기 파이어볼 저장
 for r, c, m, s, d in fireball_info:
     fireball_dict[(r - 1, c - 1)].append([m, s, d])
 
 for _ in range(K):
     # 1. 파이어볼 이동 d 방향 s 만큼 이동
-    new_fireballs = []
-    for k in fireball_dict.keys():
+    new_fireball_dict = defaultdict(deque) # 영향을 주지 않기 위함
 
-        fireballs = fireball_dict[(k[0], k[1])]
+    for (r, c), fireballs in fireball_dict.items():
         while fireballs:
             m, s, d = fireballs.popleft()
             if m == 0: continue
-            nr, nc = (k[0] + dir[d][0] * s) % N, (k[1] + dir[d][1] * s) % N
-            new_fireballs.append((nr, nc, m, s, d))
+            nr, nc = (r + dir[d][0] * s) % N, (c + dir[d][1] * s) % N
+            new_fireball_dict[(nr, nc)].append((m, s, d))
 
-    for r, c, m, s, d in new_fireballs:
-        fireball_dict[(r, c)].append((m, s, d))
+    fireball_dict = new_fireball_dict
 
     # 2. 파이어볼 2개 이상인 칸 처리
-    for k, fireballs in fireball_dict.items():
+    for (r, c), fireballs in fireball_dict.items():
         if len(fireballs) >= 2:
             tm, ts, tf = 0, 0, len(fireballs)
             odd_cnt, even_cnt = 0, 0
@@ -38,18 +38,12 @@ for _ in range(K):
                     even_cnt += 1
                 else:
                     odd_cnt += 1
-            direction = []
-            if odd_cnt == tf or even_cnt == tf:
-                direction = odd_even_dir[0]
-            else:
-                direction = odd_even_dir[1]
-            # 4방향 파이어볼 생성
-            for d in direction:
-                fireball_dict[(k[0], k[1])].append((tm // 5, ts // tf, d))
+
+            if tm // 5 > 0: # 질량 0이 되면 추가하지 않음
+                direction = odd_even_dir[0] if odd_cnt == tf or even_cnt == tf else odd_even_dir[1]
+                # 4방향 파이어볼 생성
+                for d in direction:
+                    fireball_dict[(r, c)].append((tm // 5, ts // tf, d))
 
 # 3. 파이어볼 질량의 합 처리
-result = 0
-for balls in fireball_dict.values():
-    for fire in balls:
-        result += fire[0]
-print(result)
+print(sum(m for balls in fireball_dict.values() for m, _, _ in balls))
