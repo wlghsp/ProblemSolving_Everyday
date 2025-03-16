3fc479365756f2585ea8c2ad6d77fe27b3e58614
JIHO CHOI <wlghsp@gmail.com>
2025-03-16 23:33:45 +0900

    Leetcode 15. 3Sum 풀이 예정

---
+++ LeetCode/leetcode15_3Sum/test_main.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+from main import Solution
+
+
+class TestSolution(TestCase):
+    def test_three_sum(self):
+        sol = Solution()
+        self.assertEqual(sol.threeSum([-1,0,1,2,-1,-4]), [[-1,-1,2],[-1,0,1]])
+
