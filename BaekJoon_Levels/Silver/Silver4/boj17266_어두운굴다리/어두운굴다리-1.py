70cfc1d20d0da29fd0f3c56a4fdc1f744d3a9f07
JIHO CHOI <wlghsp@gmail.com>
2025-03-27 08:48:32 +0900

    백준 17266 어두운 굴다리 풀이 ing

---
+++ BaekJoon_Levels/Silver/Silver4/boj17266_어두운굴다리/어두운굴다리-1.py
@@ -0,0 +1,7 @@
+N = int(input()) # 굴다리의 길이 N
+M = int(input()) # 가로등의 개수 M
+lamp_locs = list(map(int, input().split())) # M개의 가로등의 위치
+max_loc = max(lamp_locs) # 최대 위치
+
+# 굴다리의 길이 N 을 모두 비추기 위한 가로등의 최소 높이
+
