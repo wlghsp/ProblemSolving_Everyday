a037aff80871e300fadd2dab134d5445cb8cc127
JIHO CHOI <jh.choi2@okestro.com>
2025-03-27 13:43:52 +0900

    백준 21921 블로그 학습 완료
    - 부분합을 매번 할 경우 O(N * P) 로 시간초과 발생
    - 슬라이딩 윈도우 O(N) 풀이로 시간 초과 방지

--- BaekJoon_Levels/Silver/Silver3/boj21921_블로그/블로그-1.py
+++ BaekJoon_Levels/Silver/Silver3/boj21921_블로그/블로그-1.py
@@ -1,1 +1,21 @@
+N, X = map(int, input().split())
+visitor_cnt = list(map(int, input().split()))
 
+# 슬라이딩 윈도우
+sub_sum = sum(visitor_cnt[:X])
+max_sum = sub_sum
+max_cnt = 1
+
+for i in range(X, N):
+    sub_sum -= visitor_cnt[i - X]
+    sub_sum += visitor_cnt[i]
+    if max_sum < sub_sum:
+        max_sum = sub_sum
+        max_cnt = 1
+    elif max_sum == sub_sum:
+        max_cnt += 1
+
+if max_sum == 0:
+    print("SAD")
+else:
+    print(max_sum, max_cnt, sep='\n')
