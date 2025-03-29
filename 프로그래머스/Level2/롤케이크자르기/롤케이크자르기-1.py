0319473dd832fd40bc089eab3c90553a63551af6
JIHO CHOI <jh.choi2@okestro.com>
2025-03-29 11:08:19 +0900

    프로그래머스 롤케이크 자르기 풀이 개선

--- 프로그래머스/Level2/롤케이크자르기/롤케이크자르기-1.py
+++ 프로그래머스/Level2/롤케이크자르기/롤케이크자르기-1.py
@@ -3,28 +3,21 @@ from collections import defaultdict
 def solution(topping):
     answer = 0
     left, right = defaultdict(int), defaultdict(int)
-    left[topping[0]] += 1
-    for i in range(1, len(topping)):
-        right[topping[i]] += 1
-    if check_keys(left, right):
-        answer += 1
+    for t in topping:
+        right[t] += 1
 
-    for i in range(1, len(topping) - 1):
-        left[topping[i]] += 1
-        right[topping[i]] -= 1
+    for i in range(len(topping) - 1):
+        t = topping[i]
+        left[t] += 1
+        right[t] -= 1
 
-        if right[topping[i]] == 0:
-            right.pop(topping[i])
+        if right[t] == 0:
+            right.pop(t)
 
-        if check_keys(left, right):
+        if len(left) == len(right):
             answer += 1
 
-
     return answer
-
-def check_keys(left, right):
-    return len(left.keys()) == len(right.keys())
-
 
 print(solution([1, 2, 1, 3, 1, 4, 1, 2])) # 2
 print(solution([1, 2, 3, 1, 4])) # 0
