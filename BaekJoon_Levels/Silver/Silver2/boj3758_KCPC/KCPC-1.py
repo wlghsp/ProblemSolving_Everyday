8222620c84f165ff3801b1b667b10a02e37f8cc8
JIHO CHOI <jh.choi2@okestro.com>
2025-04-03 09:03:01 +0900

    백준 3758 KCPC 풀이 코드 개선
    - 변수명 개선
    - enumerate 인덱스 번호 전달하는데 start=1 로 1부터 시작하게 하여 rank를 나타내개 함

--- BaekJoon_Levels/Silver/Silver2/boj3758_KCPC/KCPC-1.py
+++ BaekJoon_Levels/Silver/Silver2/boj3758_KCPC/KCPC-1.py
@@ -6,25 +6,23 @@ for _ in range(T):
     # 팀의 개수 n, 문제의 개수 k, 당신 팀의 ID t, 로그 엔트리의 갯수 m
     n, k, t, m = map(int, input().split())
     logs = [list(map(int, input().split())) for _ in range(m)]
-    scores = defaultdict(int)
-    teams = {i: [0, 0, 0] for i in range(1, n + 1)}
+    best_scores = defaultdict(int)
+    team_stats = {i: [0, 0, 0] for i in range(1, n + 1)}
 
     for i in range(m):
         team_id, prob_num, score = logs[i]
-        prev = scores[(team_id, prob_num)]
-        scores[(team_id, prob_num)] = max(prev, score)
-        team = teams[team_id]
-        team[1] += 1
-        team[2] = i
+        key = (team_id, prob_num)
+        best_scores[key] = max(best_scores[key], score)
 
-    for team_id, prob_num in scores.keys():
-        team = teams[team_id]
-        team[0] += scores[(team_id, prob_num)]
+        team_stats[team_id][1] += 1
+        team_stats[team_id][2] = i
 
-    sorted_teams = sorted(teams.items(), key=lambda x: (-x[1][0], x[1][1], x[1][2]))
-    rank = 1
-    for a, _ in sorted_teams:
-        if a == t:
+    for (team_id, prob_num), score in best_scores.items():
+        team_stats[team_id][0] += score
+
+    sorted_teams = sorted(team_stats.items(), key=lambda x: (-x[1][0], x[1][1], x[1][2]))
+
+    for rank, (team_id, _) in enumerate(sorted_teams, start=1):
+        if team_id == t:
             print(rank)
             break
-        rank += 1
