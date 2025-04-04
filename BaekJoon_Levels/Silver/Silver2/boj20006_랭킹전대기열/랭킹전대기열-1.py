794b6499b5f70589274df884eca46b503aa2c389
JIHO CHOI <wlghsp@gmail.com>
2025-04-04 20:13:34 +0900

    백준 20006 랭킹전 대기열 풀이 완료
    - 생성된 순서를 정확히 지켜야하는데 딕셔너리로는 순서를 보장하기 어려우므로 리스트를 사용

--- BaekJoon_Levels/Silver/Silver2/boj20006_랭킹전대기열/랭킹전대기열-1.py
+++ BaekJoon_Levels/Silver/Silver2/boj20006_랭킹전대기열/랭킹전대기열-1.py
@@ -1,32 +1,30 @@
-def check_if_matching_room(level, M, rooms):
-    for room_level, v in rooms.items():
-        if abs(room_level - level) <= 10 and len(v) < M:
-            return True
-
-    return False
-
 
 P, M = map(int, input().split())
-rooms = {}
+rooms = []
 result = []
+
+def check_if_matching_room(level):
+    for room_level, players in rooms:
+        if abs(room_level - level) <= 10 and len(players) < M:
+            return True
+    return False
 
 for _ in range(P):
     level, nickname = input().split()
     level = int(level)
-    if len(rooms) > 0 and check_if_matching_room(level, M, rooms):
+    if check_if_matching_room(level):
         found = False
-        for room_level, v in rooms.items():
-            if not found and abs(room_level - level) <= 10 and len(v) < M:
-                v.append((level, nickname))
+        for room_level, players in rooms:
+            if not found and abs(room_level - level) <= 10 and len(players) < M:
+                players.append((level, nickname))
                 found = True
-                rooms[room_level] = v
     else:
-        rooms[int(level)] = [(level, nickname)]
+        rooms.append((level, [(level, nickname)]))
 
-for v in rooms.values():
-    result.append("Waiting!" if len(v) < M else "Started!")
-    v.sort(key=lambda x: x[1])
-    for level, nickname in v:
+for room_level, players in rooms:
+    result.append("Waiting!" if len(players) < M else "Started!")
+    players.sort(key=lambda x: x[1])
+    for level, nickname in players:
         result.append(str(level) + " " + nickname)
 
 print(*result, sep='\n')
