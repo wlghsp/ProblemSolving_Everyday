4777c2ef5b6f29c39ef672aa7956e3ed00e9719d
JIHO CHOI <jh.choi2@okestro.com>
2025-04-07 17:48:39 +0900

    프로그래머스 N진수 게임 풀이 예정

--- 프로그래머스/Level2/N진수게임/N진수게임-1.py
+++ 프로그래머스/Level2/N진수게임/N진수게임-1.py
@@ -1,6 +1,8 @@
 def solution(n, t, m, p):
     answer = ''
-
     return answer
 
-print(solution(2, 4, 2, 1)) # "0111"
+
+print(solution(2, 4, 2, 1))  # "0111"
+print(solution(16, 16, 2, 1))  # "02468ACE11111111"
+print(solution(16, 16, 2, 2))  # "13579BDF01234567"
