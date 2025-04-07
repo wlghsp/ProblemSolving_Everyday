1f46a6dab76922271f63fef5b0d8d533f4756f79
JIHO CHOI <jh.choi2@okestro.com>
2025-04-07 18:23:08 +0900

    프로그래머스 N진수 게임 풀이 완료

--- 프로그래머스/Level2/N진수게임/N진수게임-1.py
+++ 프로그래머스/Level2/N진수게임/N진수게임-1.py
@@ -1,6 +1,25 @@
 def solution(n, t, m, p):
-    answer = ''
-    return answer
+    number = 0
+
+    tmp = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    # n진수를 10진수로 변환하는 함수
+    def convert(num, base):
+        if num == 0:
+            return '0'
+        res = ''
+        while num > 0:
+            num, mod = divmod(num, base)
+            res = tmp[mod] + res
+        return res
+    total = []
+    while len(total) < (t * m):
+        total.append(convert(number, n))
+        number += 1
+    total = ''.join(total)[:t * m]
+    answer = []
+    for i in range(p - 1, t * m, m):
+        answer.append(total[i])
+    return ''.join(answer)
 
 
 print(solution(2, 4, 2, 1))  # "0111"
