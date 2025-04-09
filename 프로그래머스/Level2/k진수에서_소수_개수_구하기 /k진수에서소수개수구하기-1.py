e10f7cc1c92e74ec4c6dd46663c34b2fb117a4a1
JIHO CHOI <jh.choi2@okestro.com>
2025-04-09 12:47:50 +0900

    프로그래머스 k진수에서 소수 개수 구하기 개선
    - 0 으로 쪼갠 뒤 각각이 소수인지 판단
    - 빈문자열이 들어오는 경우 확인 필요

--- 프로그래머스/Level2/k진수에서_소수_개수_구하기 /k진수에서소수개수구하기-1.py
+++ 프로그래머스/Level2/k진수에서_소수_개수_구하기 /k진수에서소수개수구하기-1.py
@@ -16,8 +16,6 @@ def solution(n, k):
     S = convert(n, k)
 
     def is_prime(num):
-        if '0' in num:
-            return False
         num = int(num)
         if num <= 1:
             return False
@@ -26,29 +24,11 @@ def solution(n, k):
                 return False
         return True
 
-
-    def does_condition_matched(start, end):
-        #  1. 0P0처럼 소수 양쪽에 0이 있는 경우
-        if (start - 1 >= 0 and end + 1 < len(S)) and S[start - 1] == '0' and S[end + 1] == '0' and is_prime(S[start:end + 1]):
-            return True
-        # 2. P0처럼 소수 오른쪽에만 0이 있고 왼쪽에는 아무 것도 없는 경우
-        elif start == 0 and (end + 1 < len(S) and S[end + 1] == '0') and is_prime(S[start:end + 1]):
-            return True
-        # 3. 0P처럼 소수 왼쪽에만 0이 있고 오른쪽에는 아무 것도 없는 경우:
-        elif end == len(S) - 1 and (start - 1 >= 0 and S[start - 1] == '0') and is_prime(S[start:end + 1]):
-            return True
-        #  4. P처럼 소수 양쪽에 아무 것도 없는 경우
-        elif (start - 1 < 0 and end + 1 >= len(S)) and is_prime(S[start:end + 1]):
-            return True
-
-        return False
-
-
     # 2. 탐색하여 조건에 맞는 소수 찾아 갯수 리턴
-    for i in range(len(S)):
-        for j in range(i, len(S)):
-            if does_condition_matched(i, j):
-                answer += 1
+    for num in S.split('0'):
+        if not num: continue
+        if is_prime(num):
+            answer += 1
 
     return answer
 
