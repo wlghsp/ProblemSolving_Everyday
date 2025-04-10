5cfca7d3dad0cf4798b422a2ee787a0fcc8539ff
JIHO CHOI <wlghsp@gmail.com>
2025-04-10 22:30:18 +0900

    프로그래머스 주식 가격 풀이 완료

--- 프로그래머스/Level2/주식가격/주식가격-1.py
+++ 프로그래머스/Level2/주식가격/주식가격-1.py
@@ -2,8 +2,21 @@ from collections import deque
 
 
 def solution(prices):
-    q = deque()
+    queue = deque()
     answer = [0] * len(prices)
+
+    for i in range(len(prices)):
+        cur = prices[i]
+
+        while queue and cur < prices[queue[-1]]:
+            prev_idx = queue.pop()
+            answer[prev_idx] = i - prev_idx
+
+        queue.append(i)
+
+    for i in queue:
+        answer[i] = len(prices) - 1 - i
+
     return answer
 
 
