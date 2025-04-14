34bc374c1db7e61152b76379ad7346e1e4d47005
JIHO CHOI <jh.choi2@okestro.com>
2025-04-14 09:18:04 +0900

    프로그래머스 더 맵게 풀이 완료

--- 프로그래머스/Level2/더_맵게/더_맵게-1.py
+++ 프로그래머스/Level2/더_맵게/더_맵게-1.py
@@ -1,6 +1,26 @@
+import heapq
+
+
 def solution(scoville, K):
     answer = 0
-    return answer
+    heapq.heapify(scoville)
+    def morethan_K(arr):
+        for sco in scoville:
+            if sco < K:
+                return False
+        return True
+    succeeded = True
+    while not morethan_K(scoville):
+        if len(scoville) <= 1:
+            succeeded = False
+            break
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+        created = first + (second * 2)
+        heapq.heappush(scoville, created)
+        answer += 1
+
+    return answer if succeeded else -1
 
 
 print(solution([1, 2, 3, 9, 10, 12], 7)) # 2
