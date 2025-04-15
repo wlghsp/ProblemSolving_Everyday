95f897db791b1065546c82b36dbeac946da33ba3
JIHO CHOI <wlghsp@gmail.com>
2025-04-15 21:25:26 +0900

    프로그래머스 숫자 변환하기 DP bottom-up 풀이 완료

--- 프로그래머스/Level2/숫자변환하기/숫자변환하기-1.py
+++ 프로그래머스/Level2/숫자변환하기/숫자변환하기-1.py
@@ -1,7 +1,18 @@
-from collections import deque
-
 def solution(x, y, n):
-    return 0
+    dp = [-1] * (y + 1)
+    dp[x] = 0
+
+    for i in range(x, y + 1):
+        if dp[i] == -1:
+            continue
+        for next_num in (i + n, i * 2, i * 3):
+            if next_num <= y:
+                if dp[next_num] == -1: # 계산한 적이 없음
+                    dp[next_num] = dp[i] + 1 # 단순히 그전 숫자의 횟수에서 + 1
+                else: # 계산한 적이 있음
+                    dp[next_num] = min(dp[next_num], dp[i] + 1) # 둘 중에 비교
+
+    return dp[y]
 
 
 print(solution(10, 40, 5))  # 2
