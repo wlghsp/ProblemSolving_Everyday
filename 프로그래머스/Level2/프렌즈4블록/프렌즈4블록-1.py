c6ff97a6251e9e805b09506d4ec198ed7d48bbde
JIHO CHOI <wlghsp@gmail.com>
2025-05-01 09:15:46 +0900

    프로그래머스 프렌즈4블록 풀이 예정

---
+++ 프로그래머스/Level2/프렌즈4블록/프렌즈4블록-1.py
@@ -0,0 +1,7 @@
+def solution(m, n, board):
+    answer = 0
+    return answer
+
+print(solution(4, 5, ["CCBDE", "AAADE", "AAABF", "CCBBF"])) # 14
+print(solution(6, 6, ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"])) # 15
+
