d07fe7f82d79e94de00debfb99ec42476095015a
JIHO CHOI <jh.choi2@okestro.com>
2025-05-01 13:14:28 +0900

    프로그래머스 2개 이하로 다른 비트 1회 풀이

--- 프로그래머스/Level2/2개_이하로_다른_비트/2개이하로다른비트-1.py
+++ 프로그래머스/Level2/2개_이하로_다른_비트/2개이하로다른비트-1.py
@@ -1,5 +1,18 @@
 def solution(numbers):
     answer = []
+
+    for num in numbers:
+        if num % 2 == 0:
+            num += 1
+            answer.append(num)
+        else:
+            s = '0' + bin(num)[2:]
+            # 홀수에서는 오른쪽에서 01 을 찾아 10으로 변경하는 것이 2개 이하 변경하면서 최소 증가
+            i = s.rfind('01')
+            s = list(s)
+            s[i], s[i + 1] = '1', '0'
+            answer.append(int(''.join(s),2))
+
     return answer
 
 
