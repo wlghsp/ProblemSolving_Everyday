4894ced79f7fb6368cde7981865b97648aca75a6
JIHO CHOI <jh.choi2@okestro.com>
2025-05-03 16:52:17 +0900

    프로그래머스 베스트 앨범 1회 풀이

--- 프로그래머스/Level3/베스트앨범/베스트앨범-1.py
+++ 프로그래머스/Level3/베스트앨범/베스트앨범-1.py
@@ -1,5 +1,23 @@
+from collections import defaultdict
+
+
 def solution(genres, plays):
+    records = []
+    genres_total = defaultdict(int)
+    for i, (g, p) in enumerate(zip(genres, plays)):
+        genres_total[g] += p
+        records.append((g, p, i))
+
+    # 장르 수 desc, 플레이수 desc, 인덱스 asc
+    records.sort(key=lambda x: [(-genres_total[x[0]], -x[1], x[2])])
+
     answer = []
+    genres_cnt = defaultdict(int)
+    for g, p, i in records:
+        if genres_cnt[g] < 2:
+            answer.append(i)
+            genres_cnt[g] += 1
+
     return answer
 
 
