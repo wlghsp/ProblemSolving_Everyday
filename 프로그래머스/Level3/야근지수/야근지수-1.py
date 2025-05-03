ada77b6b66a0eec6515132c94094ee73d90bb964
JIHO CHOI <jh.choi2@okestro.com>
2025-05-03 16:16:11 +0900

    프로그래머스 야근 지수 1회 풀이

--- 프로그래머스/Level3/야근지수/야근지수-1.py
+++ 프로그래머스/Level3/야근지수/야근지수-1.py
@@ -1,6 +1,18 @@
-def solution(n, works):
+import heapq
 
-    return 0
+
+def solution(n, works):
+    max_heap = []
+    for w in works:
+        heapq.heappush(max_heap, (-w, w))
+    while n > 0:
+        _, w = heapq.heappop(max_heap)
+        if w > 0:
+            w -= 1
+        heapq.heappush(max_heap, (-w, w))
+        n -= 1
+
+    return sum(i ** 2 for _, i in max_heap)
 
 print(solution(4, [4, 3, 3])) # 12
 print(solution(3, [1, 1])) # 0
