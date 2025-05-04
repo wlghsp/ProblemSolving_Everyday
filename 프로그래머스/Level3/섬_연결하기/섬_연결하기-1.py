a4c33a7d35eb99ac72a01371243026f1946c9d99
JIHO CHOI <jh.choi2@okestro.com>
2025-05-04 16:18:37 +0900

    프로그래머스 섬 연결하기 풀이 완료

--- 프로그래머스/Level3/섬_연결하기/섬_연결하기-1.py
+++ 프로그래머스/Level3/섬_연결하기/섬_연결하기-1.py
@@ -1,4 +1,25 @@
 def solution(n, costs):
-  return 0
+    parent = [i for i in range(n + 1)]
 
-print(solution(4, [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]])) # 4
+    def find(x):
+        if parent[x] != x:
+            parent[x] = find(parent[x])
+        return parent[x]
+
+    def union(a, b):
+        pa = find(a)
+        pb = find(b)
+        parent[pa] = pb
+
+    def kruskal():
+        costs.sort(key=lambda x: x[2])
+        total = 0
+        for a, b, cost in costs:
+            if find(a) != find(b):
+                union(a, b)
+                total += cost
+        return total
+    return kruskal()
+
+
+print(solution(4, [[0, 1, 1], [0, 2, 2], [1, 2, 5], [1, 3, 1], [2, 3, 8]]))  # 4
