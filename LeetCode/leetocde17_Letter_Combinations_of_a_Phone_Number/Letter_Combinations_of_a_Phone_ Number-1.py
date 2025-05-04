712c1636e489ddb05b8d8e305454bc91ddb1c432
JIHO CHOI <jh.choi2@okestro.com>
2025-05-04 19:41:11 +0900

    Leetcode 17. Letter Combinations of a Phone Number 풀이 완료

--- LeetCode/leetocde17_Letter_Combinations_of_a_Phone_Number/Letter_Combinations_of_a_Phone_ Number-1.py
+++ LeetCode/leetocde17_Letter_Combinations_of_a_Phone_Number/Letter_Combinations_of_a_Phone_ Number-1.py
@@ -2,7 +2,30 @@ from typing import List
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        digit_to_letter = {
+            '2' : 'abc',
+            '3' : 'def',
+            '4' : 'ghi',
+            '5' : 'jkl',
+            '6' : 'mno',
+            '7' : 'pqrs',
+            '8' : 'tuv',
+            '9' : 'wxyz',
+        }
+
         answer = []
+        def backtrack(picked, start):
+            if len(picked) == len(digits):
+                answer.append(picked)
+                return
+
+            alphabets = digit_to_letter[digits[start]]
+            for c in alphabets:
+                backtrack(picked + c, start + 1)
+
+        backtrack("", 0)
         return answer
 
 
