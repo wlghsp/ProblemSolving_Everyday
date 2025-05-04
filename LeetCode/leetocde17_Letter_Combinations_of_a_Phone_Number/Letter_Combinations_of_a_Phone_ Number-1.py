585b2f5a6b7bf4db7f6b454ecc5410f8235b7620
JIHO CHOI <jh.choi2@okestro.com>
2025-05-04 16:02:28 +0900

    Leetcode 17. Letter Combinations of a Phone Number 풀이 예정

---
+++ LeetCode/leetocde17_Letter_Combinations_of_a_Phone_Number/Letter_Combinations_of_a_Phone_ Number-1.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        answer = []
+        return answer
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.letterCombinations("23"))  # ["ad","ae","af","bd","be","bf","cd","ce","cf"]
