8762db328c697bad37787454c8f81147ceea3431
JIHO CHOI <wlghsp@gmail.com>
2025-05-05 23:43:07 +0900

    프로그래머스 가장 먼 노드 풀이

--- 프로그래머스/Level3/가장_먼_노드/가장먼노드-1.py
+++ 프로그래머스/Level3/가장_먼_노드/가장먼노드-1.py
@@ -1,5 +1,29 @@
+from collections import deque
+
+
 def solution(n, edge):
+    graph = [[] for _ in range(n + 1)]
+    for a, b in edge:
+        graph[a].append(b)
+        graph[b].append(a)
+
+    distances = [-1] * (n + 1)
+    q = deque()
+    q.append(1)
+    distances[1] = 0
+    while q:
+        node = q.popleft()
+        for next_node in graph[node]:
+            if distances[next_node] != -1: continue
+
+            distances[next_node] = distances[node] + 1
+            q.append(next_node)
+
     answer = 0
+    max_distance = max(distances)
+    for d in distances:
+        if d == max_distance:
+            answer += 1
     return answer
 
 
