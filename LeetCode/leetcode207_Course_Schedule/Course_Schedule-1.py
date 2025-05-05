c89298c0a021c36a906a043cfe8bedb44c4d8024
JIHO CHOI <wlghsp@gmail.com>
2025-05-05 23:46:43 +0900

    Leetcode 207. Course Schedule 풀이

--- LeetCode/leetcode207_Course_Schedule/Course_Schedule-1.py
+++ LeetCode/leetcode207_Course_Schedule/Course_Schedule-1.py
@@ -4,7 +4,27 @@ from typing import List
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        return False
+        indgree = [0] * numCourses
+        graph = [[] for _ in range(numCourses)]
+        for a, b in prerequisites:
+            graph[b].append(a)
+            indgree[a] += 1
+
+        q = deque()
+        for i in range(numCourses):
+            if indgree[i] == 0:
+                q.append(i)
+        result = []
+        while q:
+            node = q.popleft()
+            result.append(node)
+
+            for neighbor in graph[node]:
+                indgree[neighbor] -= 1
+                if indgree[neighbor] == 0:
+                    q.append(neighbor)
+
+        return True if len(result) == numCourses else False
 
 
 if __name__ == "__main__":
