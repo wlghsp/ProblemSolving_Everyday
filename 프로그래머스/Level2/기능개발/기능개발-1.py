8a5d5c61ba28906d55f1ccbd6ea41c4f9b3685be
JIHO CHOI <jh.choi2@okestro.com>
2025-05-05 17:47:28 +0900

    프로그래머스 기능 개발 풀이 예정

---
+++ 프로그래머스/Level2/기능개발/기능개발-1.py
@@ -0,0 +1,6 @@
+def solution(progresses, speeds):
+    answer = []
+    return answer
+
+print(solution([93, 30, 55], [1, 30, 5])) # [2, 1]
+print(solution([95, 90, 99, 99, 80, 99]	, [1, 1, 1, 1, 1, 1]	)) # [1, 3, 2]
