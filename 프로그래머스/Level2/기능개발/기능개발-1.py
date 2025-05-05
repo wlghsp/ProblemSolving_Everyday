dd4da433778860ca4fb4b3bf3f1c055fb5556e40
JIHO CHOI <wlghsp@gmail.com>
2025-05-05 23:52:01 +0900

    프로그래머스 기능 개발 풀이 완료

--- 프로그래머스/Level2/기능개발/기능개발-1.py
+++ 프로그래머스/Level2/기능개발/기능개발-1.py
@@ -1,5 +1,26 @@
+import math
+from collections import deque
+
+
 def solution(progresses, speeds):
     answer = []
+    finish_time = deque()
+
+    # 1. 100 % 까지 걸리는 기간 기록
+    for progress, speed in zip(progresses, speeds):
+        left = (100 - progress)
+        days = math.ceil(left / speed)
+        finish_time.append(days)
+
+    # 2. 걸리는 시간으로 그룹별 카운트
+    while finish_time:
+        fun = finish_time.popleft()
+        cnt = 1
+        while finish_time and finish_time[0] <= fun:
+            finish_time.popleft()
+            cnt += 1
+        answer.append(cnt)
+
     return answer
 
 print(solution([93, 30, 55], [1, 30, 5])) # [2, 1]
