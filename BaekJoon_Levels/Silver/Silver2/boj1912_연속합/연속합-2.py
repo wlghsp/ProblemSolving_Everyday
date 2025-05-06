20498ae1b5c5b073c6560e20dfa4dd18bd37650e
JIHO CHOI <jh.choi2@okestro.com>
2025-05-06 20:54:29 +0900

    백준 1912 연속합 풀이 완료

---
+++ BaekJoon_Levels/Silver/Silver2/boj1912_연속합/연속합-2.py
@@ -0,0 +1,10 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+N = int(input())
+arr = list(map(int, input().split()))
+
+dp = [0] * N
+for i in range(N):
+    dp[i] = max(arr[i], dp[i - 1] + arr[i])
+print(max(dp))
