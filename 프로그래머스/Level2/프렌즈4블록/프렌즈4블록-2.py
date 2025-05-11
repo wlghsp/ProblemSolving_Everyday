127e0989bd8f3c4701dc1737b38c247947025e5b
JIHO CHOI <wlghsp@gmail.com>
2025-05-11 21:56:08 +0900

    프로그래머스 프렌즈4블록 풀이 완료

--- 프로그래머스/Level2/프렌즈4블록/프렌즈4블록-2.py
+++ 프로그래머스/Level2/프렌즈4블록/프렌즈4블록-2.py
@@ -1,5 +1,41 @@
 def solution(m, n, board):
     answer = 0
+    l_board = []
+    for b in board:
+        l_board.append(list(b))
+    def get_delete():
+        delete_loc = set()
+        for i in range(m - 1):
+            for j in range(n - 1):
+                cur = l_board[i][j]
+                if cur == '0': continue
+
+                if cur == l_board[i + 1][j] == l_board[i][j + 1] == l_board[i + 1][j + 1]:
+                    delete_loc.update(((i, j), (i + 1, j), (i, j + 1), (i + 1, j + 1)))
+        return delete_loc
+    while True:
+        to_delete = get_delete()
+
+        if not to_delete:
+            break
+
+        answer += len(to_delete)
+
+        for x, y in to_delete:
+            l_board[x][y] = '0'
+
+        for col in range(n):
+            stack = []
+            for row in range(m):
+                if l_board[row][col] != '0':
+                    stack.append(l_board[row][col])
+
+            for row in range(m - 1, -1, -1):
+                if stack:
+                    l_board[row][col] = stack.pop()
+                else:
+                    l_board[row][col] = '0'
+
     return answer
 
 
