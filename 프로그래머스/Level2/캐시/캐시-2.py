cfba2bfac834aaf6b35defaaaf542e8a91fd4e32
JIHO CHOI <wlghsp@gmail.com>
2025-05-11 21:45:28 +0900

    프로그래머스 캐시 풀이 완료

--- 프로그래머스/Level2/캐시/캐시-2.py
+++ 프로그래머스/Level2/캐시/캐시-2.py
@@ -1,6 +1,22 @@
 def solution(cacheSize, cities):
     answer = 0
+    cache = []
+    for city in cities:
+        city = city.lower()
+        if city in cache:
+            cache.remove(city)
+            cache.append(city)
+            answer += 1
+        else: # 캐시 미스
+            if cache and len(cache) >= cacheSize: # 가득 찬 상태이면
+                cache.pop(0)
+                cache.append(city)
+            elif cacheSize > len(cache):
+                cache.append(city)
+
+            answer += 5
+
     return answer
 
-print(solution(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
-# 50
+print(solution(2, ["Jeju", "Pangyo", "NewYork", "newyork"]))
+print(solution(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
