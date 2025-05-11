7d4a7bfcf3d9325cab0f9d17873a82c7f2efe01a
JIHO CHOI <jh.choi2@okestro.com>
2025-05-11 14:42:13 +0900

    프로그래머스 파일명 정렬 풀이 예정

---
+++ 프로그래머스/Level2/파일명정렬/파일명정렬-1.py
@@ -0,0 +1,7 @@
+def solution(files):
+    answer = []
+    return answer
+
+
+print(solution(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]))
+# ["img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"]
