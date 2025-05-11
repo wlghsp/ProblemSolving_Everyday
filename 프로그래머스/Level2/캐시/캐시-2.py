3fbd75b3c669237a28e5708b59b7a2457cb35a13
JIHO CHOI <jh.choi2@okestro.com>
2025-05-11 15:18:04 +0900

    프로그래머스 캐시 풀이 예정

---
+++ 프로그래머스/Level2/캐시/캐시-2.py
@@ -0,0 +1,6 @@
+def solution(cacheSize, cities):
+    answer = 0
+    return answer
+
+print(solution(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
+# 50
