c986e5e31c45b0eabcb74be9c48098478d6d5c29
JIHO CHOI <jh.choi2@okestro.com>
2025-05-12 19:20:07 +0900

    프로그래머스 오픈채팅방 풀이 예정

---
+++ 프로그래머스/Level2/오픈채팅방/오픈채팅방-1.py
@@ -0,0 +1,7 @@
+def solution(record):
+    answer = []
+    return answer
+
+
+print(solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]))
+# ["Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."]
