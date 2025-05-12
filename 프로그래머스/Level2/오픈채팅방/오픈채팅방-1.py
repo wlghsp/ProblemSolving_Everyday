9a730bc2543108d3d8ffa905eec7e72526a5747a
JIHO CHOI <wlghsp@gmail.com>
2025-05-12 22:54:20 +0900

    프로그래머스 오픈 채팅방 풀이 완료

--- 프로그래머스/Level2/오픈채팅방/오픈채팅방-1.py
+++ 프로그래머스/Level2/오픈채팅방/오픈채팅방-1.py
@@ -1,5 +1,17 @@
 def solution(record):
     answer = []
+    id_name = {}
+    for r in record:
+        r = r.split()
+        if r[0] in ['Enter', 'Change']:
+            id_name[r[1]] = r[2]
+
+    for r in record:
+        r = r.split()
+        if r[0] == 'Enter':
+            answer.append(id_name[r[1]] + '님이 들어왔습니다.')
+        elif r[0] == 'Leave':
+            answer.append(id_name[r[1]] + '님이 나갔습니다.')
     return answer
 
 
