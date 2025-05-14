a97ea0862f2d4afed02d5a8a2107a9583b810e48
JIHO CHOI <jh.choi2@okestro.com>
2025-05-14 16:26:36 +0900

    프로그래머스 연속된 부분 수열의 합 풀이 완료

--- 프로그래머스/Level2/연속된부분수열의합/연속된부분수열의합-1.py
+++ 프로그래머스/Level2/연속된부분수열의합/연속된부분수열의합-1.py
@@ -1,6 +1,17 @@
 def solution(sequence, k):
     answer = []
-    return answer
+    left = 0
+    total = 0
+    for right in range(len(sequence)):
+        total += sequence[right]
+        while total > k:
+            total -= sequence[left]
+            left += 1
+        if total == k:
+            answer.append([right - left + 1, left, right])
+    answer.sort(key= lambda x: [x[0], x[1]])
+
+    return [answer[0][1], answer[0][2]]
 
 print(solution([1, 2, 3, 4, 5], 7))
 
