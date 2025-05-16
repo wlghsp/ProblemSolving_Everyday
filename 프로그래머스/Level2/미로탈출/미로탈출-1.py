7df29fe4c844584106da4781db1f382921e02dab
JIHO CHOI <jh.choi2@okestro.com>
2025-05-16 20:08:53 +0900

    프로그래머스 미로 탈출 풀이 예정

---
+++ 프로그래머스/Level2/미로탈출/미로탈출-1.py
@@ -0,0 +1,9 @@
+def solution(maps):
+    answer = 0
+    return answer
+
+
+
+print(solution(["SOOOL","XXXXO","OOOOO","OXXXX","OOOOE"])) # 16
+print(solution(["LOOXS","OOOOX","OOOOO","OOOOO","EOOOO"])) # -1
+
