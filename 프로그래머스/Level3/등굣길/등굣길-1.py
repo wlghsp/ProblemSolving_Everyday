4e3729e03479964a6e0fe27521df506e677f760a
JIHO CHOI <jh.choi2@okestro.com>
2025-05-16 18:00:26 +0900

    프로그래머스 등굣길 풀이 완료

--- 프로그래머스/Level3/등굣길/등굣길-1.py
+++ 프로그래머스/Level3/등굣길/등굣길-1.py
@@ -1,5 +1,25 @@
 def solution(m, n, puddles):
-    answer = 0
-    return answer
+    dp = [[-1] * m for _ in range(n)]
+    impossible = [[False] * m for _ in range(n)]
+    for y, x in puddles:
+        impossible[x - 1][y - 1] = True
+    def dfs(x, y):
+        if x == n - 1 and y == m - 1:
+            return 1
+        if dp[x][y] != -1:
+            return dp[x][y]
+
+        dp[x][y] = 0
+
+        for dx, dy in [(1, 0), (0, 1)]:
+            nx, ny = x + dx, y + dy
+            if not (0 <= nx < n and 0 <= ny < m): continue
+            if impossible[nx][ny]: continue
+
+            dp[x][y] = (dp[x][y] + dfs(nx, ny)) % 1000000007
+        return dp[x][y]
+
+
+    return dfs(0, 0)
 
 print(solution(4, 3, [[2, 2]])) # 4
