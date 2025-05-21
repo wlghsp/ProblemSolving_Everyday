e8dada2e02bb6fc3375860ac19e28dcc36ee2476
JIHO CHOI <jh.choi2@okestro.com>
2025-05-21 20:15:00 +0900

    백준 2110 공유기 설치 풀이 완료

--- BaekJoon_Levels/Gold/Gold4/boj2110_공유기_설치/공유기설치-1.py
+++ BaekJoon_Levels/Gold/Gold4/boj2110_공유기_설치/공유기설치-1.py
@@ -1,2 +1,28 @@
 N, C = map(int, input().split())
 houses = [int(input()) for _ in range(N)]
+houses.sort()
+
+def can_install(distance):
+    count = 1
+    last = houses[0]
+    for i in range(1, N):
+        if houses[i] - last >= distance:
+            count += 1
+            last = houses[i]
+        if count >= C:
+            return True
+
+    return False
+
+ans = 0
+l, r = 1, (houses[-1] - houses[0])
+while l <= r:
+    mid = l + (r - l) // 2
+
+    if can_install(mid):
+        ans = mid
+        l = mid + 1
+    else:
+        r = mid - 1
+
+print(ans)
