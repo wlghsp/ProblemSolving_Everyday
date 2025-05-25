8af825149c30fba53e05d9cac794e7b901afdf4d
JIHO CHOI <wlghsp@gmail.com>
2025-05-25 21:00:56 +0900

    leetcode 5. Longest Palindromic Substring 풀이 예정

---
+++ LeetCode/leetcode5_Longest_Palindromic_Substring/Longest_Palindromic_Substring-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.longestPalindrome("babad")) # "bab"
