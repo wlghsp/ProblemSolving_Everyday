c2be8683ef1f1237304efce0923e81a8a675be3a
JIHO CHOI <wlghsp@gmail.com>
2025-05-26 08:41:26 +0900

    프로그래머스 괄호 변환 풀이 완료

--- 프로그래머스/Level2/괄호변환/괄호변환-1.py
+++ 프로그래머스/Level2/괄호변환/괄호변환-1.py
@@ -1,8 +1,53 @@
-def solution(p):
-    answer = ''
 
+def is_correct(s):
+    cnt = 0
+    for c in s:
+        if c == ')':
+            if cnt <= 0:
+                return False
+            else:
+                cnt -= 1
+        else:
+            cnt += 1
+    return True
+
+def reverse(s):
+    res = ''
+    for c in s:
+        if c == ')':
+            res += '('
+        else:
+            res += ')'
+    return res
+def split_into_uv(s):
+    count = 0
+    u, v = '', ''
+    for i in range(len(s)):
+        if s[i] == '(':
+            count += 1
+        else:
+            count -= 1
+        if count == 0:
+            u = s[:i + 1]
+            v = s[i + 1:]
+            return u, v
+    return u, v
+
+
+def solution(p):
+    if not p:
+        return ''
+    u, v = split_into_uv(p)
+
+    if is_correct(u):
+        return u + solution(v)
+
+    answer = '('
+    answer += solution(v)
+    answer += ')'
+    answer += reverse(u[1:-1])
     return answer
 
 print(solution("(()())()")) # "(()())()"
-# print(solution(")(")) # "()"
-# print(solution("()))((()")) # "(()())()"
+print(solution(")(")) # "()"
+print(solution("()))((()")) # "()(())()"
