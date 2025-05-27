8b578d2986e0a5ce9a88aa7b6a132196068267a2
JIHO CHOI <wlghsp@gmail.com>
2025-05-27 09:06:15 +0900

    백준 2668 숫자 고르기 풀이 완료

--- BaekJoon_Levels/Gold/Gold5/boj2668_숫자고르기/숫자고르기-1.py
+++ BaekJoon_Levels/Gold/Gold5/boj2668_숫자고르기/숫자고르기-1.py
@@ -4,5 +4,26 @@ sys.setrecursionlimit(10 ** 6)
 input = lambda : sys.stdin.readline().rstrip()
 
 N = int(input())
-nums = [int(input()) for _ in range(N)]
+nums = [0] + [int(input()) for _ in range(N)]
+
+res = []
+def dfs(start, curr, path, visited):
+    visited[curr] = True
+    path.append(curr)
+
+    next_node = nums[curr]
+    if not visited[next_node]:
+        dfs(start, next_node, path, visited)
+    elif next_node == start:
+        res.extend(path)
+
+
+for i in range(1, N + 1):
+    visited = [False] * (N + 1)
+    path = []
+    dfs(i, i, path, visited)
+
+res = sorted(set(res))
+print(len(res))
+print(*res, sep='\n')
 
