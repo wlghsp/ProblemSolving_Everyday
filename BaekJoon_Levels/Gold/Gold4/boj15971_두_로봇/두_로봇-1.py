b79344ef102e48760349aa3d3ae4d12c581ee5e5
JIHO CHOI <wlghsp@gmail.com>
2025-05-28 19:34:35 +0900

    백준 15971 두 로봇 풀이 완료

--- BaekJoon_Levels/Gold/Gold4/boj15971_두_로봇/두_로봇-1.py
+++ BaekJoon_Levels/Gold/Gold4/boj15971_두_로봇/두_로봇-1.py
@@ -1,4 +1,31 @@
 import sys
+
 sys.setrecursionlimit(10 ** 6)
-input = lambda : sys.stdin.readline().rstrip()
+input = lambda: sys.stdin.readline().rstrip()
+
 N, A, B = map(int, input().split())
+graph = {i: [] for i in range(1, N + 1)}
+
+for _ in range(N - 1):
+    a, b, dist = map(int, input().split())
+    graph[a].append((b, dist))
+    graph[b].append((a, dist))
+
+visited = [False] * (N + 1)
+ans = float('inf')
+
+def dfs(curr, target, total, max_dist):
+    global ans
+    visited[curr] = True
+
+    if curr == target:
+        ans = min(ans, total - max_dist)
+        return
+
+    for next, d in graph[curr]:
+        if visited[next]: continue
+
+        dfs(next, target, total + d, max(max_dist, d))
+
+dfs(A, B, 0, 0)
+print(ans)
