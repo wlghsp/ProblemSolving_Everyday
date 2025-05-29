794ac98f604d523fed9c9ffdc4991105e3bdea13
JIHO CHOI <wlghsp@gmail.com>
2025-05-29 23:11:21 +0900

    백준 1753 최단 경로 풀이 완료

--- BaekJoon_Levels/Gold/Gold4/boj1753_최단경로/최단경로-1.py
+++ BaekJoon_Levels/Gold/Gold4/boj1753_최단경로/최단경로-1.py
@@ -1,3 +1,4 @@
+import heapq
 import sys
 
 input = lambda : sys.stdin.readline().rstrip()
@@ -7,3 +8,31 @@ graph = {i: [] for i in range(V + 1)}
 INF = int(1e9)
 for _ in range(E):
     u, v, w = map(int, input().split())
+    graph[u].append((v, w))
+
+distance = [INF] * (V + 1)
+
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+
+        if distance[now] < dist:
+            continue
+
+        for v, d in graph[now]:
+            cost = dist + d
+            if cost < distance[v]:
+                distance[v] = cost
+                heapq.heappush(q, (distance[v], v))
+dijkstra(K)
+
+for i in range(1, V + 1):
+    if distance[i] == INF:
+        print("INF")
+    else:
+        print(distance[i])
