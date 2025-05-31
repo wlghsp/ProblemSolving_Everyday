02317452aaed9f68709aa246d0ae3bc8eb428df1
JIHO CHOI <wlghsp@gmail.com>
2025-05-31 11:37:02 +0900

    백준 13913 숨바꼭질 4 풀이 완료

--- BaekJoon_Levels/Gold/Gold4/boj13913_숨바꼭질4/숨바꼭질4-1.py
+++ BaekJoon_Levels/Gold/Gold4/boj13913_숨바꼭질4/숨바꼭질4-1.py
@@ -1,3 +1,39 @@
+from collections import deque
+
 N, K = map(int, input().split())
 
+prev = [0] * 100001
 
+def bfs():
+    visited = [False] * 100001
+    q = deque([N])
+    visited[N] = True
+
+    while q:
+        x = q.popleft()
+
+        if x == K:
+            print_answer()
+            return
+
+        for nx in [x - 1, x + 1, 2 * x]:
+            if not (0 <= nx <= 100000): continue
+            if visited[nx]: continue
+
+            visited[nx] = True
+            prev[nx] = x
+            q.append(nx)
+
+def print_answer():
+    path = []
+    curr = K
+    while curr != N:
+        path.append(curr)
+        curr = prev[curr]
+    path.append(N)
+    path.reverse()
+    print(len(path) - 1)
+    print(*path, sep=' ')
+
+
+bfs()
