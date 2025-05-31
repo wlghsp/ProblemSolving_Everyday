fbc4fb4ea290c7a54f79a606ed8fc4b56d67e95e
JIHO CHOI <wlghsp@gmail.com>
2025-05-31 11:14:24 +0900

    백준 1389 케빈베이컨의 6단계 법칙 풀이 완료

--- BaekJoon_Levels/Silver/Silver1/boj1389_케빈베이컨의6단계법칙/케빈베이컨의6단계법칙-2.py
+++ BaekJoon_Levels/Silver/Silver1/boj1389_케빈베이컨의6단계법칙/케빈베이컨의6단계법칙-2.py
@@ -1,7 +1,39 @@
 import sys
+from collections import deque
 
 input = lambda : sys.stdin.readline().rstrip()
 
 N, M = map(int, input().split())
+graph = {i: [] for i in range(1, N + 1)}
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def bfs(start):
+    distance = [-1] * (N + 1)
+    q = deque([start])
+    distance[start] = 0
 
 
+    while q:
+        x = q.popleft()
+
+        for nx in graph[x]:
+            if distance[nx] != -1: continue
+
+            distance[nx] = distance[x] + 1
+            q.append(nx)
+
+    return sum(distance)
+
+min_dist = float('inf')
+min_idx = 0
+
+for i in range(1, N + 1):
+    res = bfs(i)
+    if min_dist > res:
+        min_dist = res
+        min_idx = i
+
+print(min_idx)
