47c909e66699321b54e5e18633f24ed27ffa7fa6
JIHO CHOI <wlghsp@gmail.com>
2025-06-01 17:02:28 +0900

    Trie 예제 코드

---
+++ 깜지/trie_ex.py
@@ -0,0 +1,32 @@
+class TrieNode:
+    def __init__(self):
+        self.children = dict()
+        self.is_end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+            node = node.children[ch]
+        node.is_end = True
+
+    def search(self, word):
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                return False
+            node = node.children[ch]
+        return node.is_end
+
+    def starts_with(self, prefix):
+        node = self.root
+        for ch in prefix:
+            if ch not in node.children:
+                return False
+            node = node.children[ch]
+        return True
