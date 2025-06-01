070d7366e051391e475cbea55b4fc853a5e2fd90
JIHO CHOI <wlghsp@gmail.com>
2025-06-01 21:52:11 +0900

    leetcode 26. Remove Duplicates from Sorted Array 재학습
    투포인터 풀이
    
    다른 값이 나오면 left를 증가시키고 left 에 right 위치의 값을 삽입

--- LeetCode/leetcode26_Remove_Duplicates_from_Sorted_Array/RemoveDuplicatesFromSortedArray-1.py
+++ LeetCode/leetcode26_Remove_Duplicates_from_Sorted_Array/RemoveDuplicatesFromSortedArray-1.py
@@ -3,7 +3,12 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-       pass
+        left = 0
+        for right in range(1, len(nums)):
+            if nums[left] != nums[right]:
+                left += 1
+                nums[left] = nums[right]
+        return left + 1
 
 if __name__ == "__main__":
     sol = Solution()
