a1104bad4e1476efb24ad9c455ac49978ea38019
JIHO CHOI <jh.choi2@okestro.com>
2025-06-02 08:43:39 +0900

    프로그래머스 가사 검색, 자물쇠와 열쇠 풀이 예정

---
+++ 프로그래머스/Level3/가사검색/가사검색-1.py
@@ -0,0 +1,8 @@
+def solution(words, queries):
+    answer = []
+    return answer
+
+
+print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"],
+               ["fro??", "????o", "fr???", "fro???", "pro?"]))
+# [3, 2, 4, 1, 0]
