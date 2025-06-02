7c343a20049493ca99190d50a66f184a8aaf958a
JIHO CHOI <jh.choi2@okestro.com>
2025-06-02 15:53:06 +0900

    프로그래머스 문자열 압축 풀이 완료

--- 프로그래머스/Level2/문자열압축/문자열압축-1.py
+++ 프로그래머스/Level2/문자열압축/문자열압축-1.py
@@ -1,5 +1,30 @@
+def split_s(s, length):
+    res = []
+    idx = 0
+    while idx < len(s):
+        res.append(s[idx:idx + length])
+        idx += length
+    return res
+
+def compress(splits):
+    res = []
+    idx = 0
+    while idx < len(splits):
+        cnt = 1
+        curr = splits[idx]
+        idx += 1
+        while idx < len(splits) and curr == splits[idx]:
+            cnt += 1
+            idx += 1
+        res.append(curr if cnt == 1 else (str(cnt) + curr))
+    return ''.join(res)
+
 def solution(s):
-    answer = 0
+    answer = len(s)
+    for i in range(1, len(s)):
+        splits = split_s(s, i)
+        compressed = compress(splits)
+        answer = min(answer, len(compressed))
     return answer
 
 print(solution("aabbaccc")) # 7
