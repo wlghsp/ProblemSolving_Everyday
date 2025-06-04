1c8135f9382f07987b4ddd06c8d59d68d846f8f5
JIHO CHOI <jh.choi2@okestro.com>
2025-06-04 12:37:24 +0900

    백준 4485 녹색 옷 입은 애가 젤다지? 풀이 완료

--- BaekJoon_Levels/Gold/Gold4/boj4485_녹색옷입은애가젤다지/녹색옷입은애가젤다지-1.py
+++ BaekJoon_Levels/Gold/Gold4/boj4485_녹색옷입은애가젤다지/녹색옷입은애가젤다지-1.py
@@ -3,3 +3,33 @@ import sys
 
 input = lambda : sys.stdin.readline().rstrip()
 
+INF = int(1e9)
+def dijkstra():
+    distance = [[INF] * N for _ in range(N)]
+    q = []
+    heapq.heappush(q, (0, 0, rupees[0][0]))
+    distance[0][0] = rupees[0][0]
+
+    while q:
+        x, y, dist = heapq.heappop(q)
+
+        if dist > distance[x][y]: continue
+
+        for dx, dy in [(1, 0),(0, 1), (-1, 0), (0, -1)]:
+            nx, ny = x + dx, y + dy
+            if not (0 <= nx < N and 0 <= ny < N): continue
+
+            cost = dist + rupees[nx][ny]
+            if cost < distance[nx][ny]:
+                distance[nx][ny] = cost
+                heapq.heappush(q, (nx, ny, distance[nx][ny]))
+    return distance[N - 1][N - 1]
+idx = 1
+while True:
+    N = int(input())
+    if N == 0:
+        break
+    rupees = [list(map(int, input().split())) for _ in range(N)]
+    res = dijkstra()
+    print(f'Problem {idx}: {res}')
+    idx += 1
