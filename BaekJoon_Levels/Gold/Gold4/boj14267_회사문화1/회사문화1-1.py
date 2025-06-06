7b297326d40b19e52e2fc3939e6ac74b244f0d97
JIHO CHOI <wlghsp@gmail.com>
2025-06-06 21:07:59 +0900

    백준 14267 회사 문화 1 풀이 예정

--- BaekJoon_Levels/Gold/Gold4/boj14267_회사문화1/회사문화1-1.py
+++ BaekJoon_Levels/Gold/Gold4/boj14267_회사문화1/회사문화1-1.py
@@ -3,4 +3,17 @@ import sys
 input = lambda : sys.stdin.readline().rstrip()
 
 N, M = map(int, input().split())
-graph = {i: [] for i in range(N)}
+graph = {i: [] for i in range(N + 1)}
+boss = [0] + list(map(int, input().split()))
+compliments = [0] * (N + 1)
+
+for i in range(1, N + 1):
+    graph[i].append(boss[i])
+
+
+def dfs(x):
+    pass
+
+
+for _ in range(M):
+    i, w = map(int, input().split())
