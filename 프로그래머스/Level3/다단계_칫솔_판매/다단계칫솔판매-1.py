fceb83615d9166df0b6a3afc843ef5bce61e077d
JIHO CHOI <wlghsp@gmail.com>
2025-06-08 11:42:03 +0900

    프로그래머스 다단계 칫솔 판매 풀이 예정

---
+++ 프로그래머스/Level3/다단계_칫솔_판매/다단계칫솔판매-1.py
@@ -0,0 +1,10 @@
+def solution(enroll, referral, seller, amount):
+    answer = []
+    return answer
+
+
+print(solution(["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"],
+               ["-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"],
+               ["young", "john", "tod", "emily", "mary"],
+               [12, 4, 2, 5, 10]
+               ))
