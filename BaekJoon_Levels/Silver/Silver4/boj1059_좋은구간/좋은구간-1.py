34263916ef2bef25fb2070006a97ab74a7dc8f5d
JIHO CHOI <wlghsp@gmail.com>
2025-06-09 20:04:31 +0900

    백준 1059 좋은 구간 풀이 완료

--- BaekJoon_Levels/Silver/Silver4/boj1059_좋은구간/좋은구간-1.py
+++ BaekJoon_Levels/Silver/Silver4/boj1059_좋은구간/좋은구간-1.py
@@ -1,12 +1,22 @@
-
-
 L = int(input())
 S = list(map(int, input().split()))
 n = int(input())
 S.sort()
-distinct_s = set(S)
+
 res = 0
+end = S[0] - 1
+start = 1
 
+if n > end:
+    for i in range(1, len(S)):
+        if n < S[i]:
+            end = S[i] - 1
+            start = S[i - 1] + 1
+            break
 
+for i in range(start, end + 1):
+    for j in range(i + 1, end + 1):
+        if i <= n <= j:
+            res += 1
 
 print(res)
