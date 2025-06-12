c925a6094afe8cc1f9cf426fb7527eb73aa29908
JIHO CHOI <wlghsp@gmail.com>
2025-06-12 23:27:07 +0900

    백준 1717 집합의 표현 풀이 완료

--- BaekJoon_Levels/Gold/Gold5/boj1717_집합의표현/집합의표현-3.py
+++ BaekJoon_Levels/Gold/Gold5/boj1717_집합의표현/집합의표현-3.py
@@ -1,1 +1,27 @@
+import sys
 
+sys.setrecursionlimit(10 ** 6)
+input = lambda: sys.stdin.readline().rstrip()
+
+N, M = map(int, input().split())
+parents = [i for i in range(N + 1)]
+
+
+def find(x):
+    if parents[x] != x:
+        parents[x] = find(parents[x])
+    return parents[x]
+
+
+def union(a, b):
+    pa = find(a)
+    pb = find(b)
+    parents[pa] = pb
+
+
+for _ in range(M):
+    c, a, b = map(int, input().split())
+    if c == 0:
+        union(a, b)
+    elif c == 1:
+        print("YES" if find(a) == find(b) else "NO")
