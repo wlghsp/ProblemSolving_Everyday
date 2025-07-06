719bf784f52bfb22439992a4908904c92df1a63f
JIHO CHOI <jh.choi2@okestro.com>
2025-07-06 16:55:22 +0900

    Leetcode 146. LRU Cache 풀이

--- LeetCode/leetcode146_LRU_Cache/LRU_Cache-1.py
+++ LeetCode/leetcode146_LRU_Cache/LRU_Cache-1.py
@@ -1,13 +1,35 @@
+from typing import OrderedDict
+
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        pass
+        self.cache = OrderedDict()
+        self.capacity = capacity
     def get(self, key: int) -> int:
-        pass
+        if key not in self.cache:
+            return -1
+        # key 사용됐으므로 가장 뒤로 이동
+        self.cache.move_to_end(key)
+        return self.cache[key]
     def put(self, key: int, value: int) -> None:
-        pass
+        if key in self.cache:
+            # key 갱신 -> 가장 뒤로 이동
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            # 가장 오래된 항목 제거
+            self.cache.popitem(last=False)
 
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+if __name__ == "__main__":
+    obj = LRUCache(2)
+    obj.put(1, 1)
+    obj.put(2, 2)
+    print(obj.get(1))
+    obj.put(3, 3)
+    print(obj.get(2))
+    obj.put(4, 4)
+    print(obj.get(1))
+    print(obj.get(3))
+    print(obj.get(4))
+
