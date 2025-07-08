c1ffdbae69d9a800f21623fe73978504dab97619
JIHO CHOI <jh.choi2@okestro.com>
2025-07-08 13:28:05 +0900

    Leetcode 146. LRU Cache 풀이 완료

--- LeetCode/leetcode146_LRU_Cache/LRU_Cache-1.py
+++ LeetCode/leetcode146_LRU_Cache/LRU_Cache-1.py
@@ -1,11 +1,52 @@
+class Node:
+    def __init__(self, key: int, value: int):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        pass
+        self.capacity = capacity
+        self.cache = dict()
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+    def _insert(self, node):
+        prev = self.tail.prev
+        prev.next = node
+        node.prev = prev
+        node.next = self.tail
+        self.tail.prev = node
+
+    def _remove(self, node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+
     def get(self, key: int) -> int:
-        pass
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+        self._remove(node)
+        self._insert(node)
+        return self.cache[key].value
+
     def put(self, key: int, value: int) -> None:
-        pass
+        if key in self.cache:
+            self._remove(self.cache[key])
+
+        node = Node(key, value)
+        self._insert(node)
+        self.cache[key] = node
+
+        if len(self.cache) > self.capacity:
+            lru = self.head.next
+            self._remove(lru)
+            del self.cache[lru.key]
 
 if __name__ == "__main__":
     obj = LRUCache(2)
