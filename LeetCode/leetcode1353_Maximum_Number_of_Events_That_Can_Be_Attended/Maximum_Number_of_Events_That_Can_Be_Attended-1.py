5dec2ec1c6de54a9600aef9f914a0b981c278993
JIHO CHOI <jh.choi2@okestro.com>
2025-07-08 11:19:06 +0900

    Leetcode 1353. Maximum Number of Events That can be attended 풀이 완료

--- LeetCode/leetcode1353_Maximum_Number_of_Events_That_Can_Be_Attended/Maximum_Number_of_Events_That_Can_Be_Attended-1.py
+++ LeetCode/leetcode1353_Maximum_Number_of_Events_That_Can_Be_Attended/Maximum_Number_of_Events_That_Can_Be_Attended-1.py
@@ -4,7 +4,24 @@ from typing import List
 
 class Solution:
     def maxEvents(self, events: List[List[int]]) -> int:
-       pass
+        n = len(events)
+        max_day = max(end for _, end in events)
+        events.sort()
+        event_idx, cnt = 0, 0
+        pq = []
+        for day in range(1, max_day + 1):
+            while event_idx < n and events[event_idx][0] == day:
+                heapq.heappush(pq, events[event_idx][1])
+                event_idx += 1
+
+            while pq and pq[0] < day:
+                heapq.heappop(pq)
+
+            if pq:
+                heapq.heappop(pq)
+                cnt += 1
+
+        return cnt
 
 
 if __name__ == "__main__":
