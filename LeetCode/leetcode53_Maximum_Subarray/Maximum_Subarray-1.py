257a110ec380afc549ce3af8a36d839f432222e6
JIHO CHOI <jh.choi2@okestro.com>
2025-07-27 19:28:59 +0900

    leetcode 53. Maximum Subarray 풀이 완료

--- LeetCode/leetcode53_Maximum_Subarray/Maximum_Subarray-1.py
+++ LeetCode/leetcode53_Maximum_Subarray/Maximum_Subarray-1.py
@@ -3,8 +3,13 @@ from typing import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        pass
+        N = len(nums)
+        dp = [0] * N
+        dp[0] = nums[0]
+        for i in range(1, N):
+            dp[i] = max(nums[i], dp[i - 1] + nums[i])
 
+        return max(dp)
 
 if __name__ == "__main__":
     sol = Solution()
