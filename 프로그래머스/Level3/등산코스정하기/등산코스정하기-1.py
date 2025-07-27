877ed4e614e7976b6173a232e16bec86ea9cd295
JIHO CHOI <jh.choi2@okestro.com>
2025-07-27 19:09:39 +0900

    프로그래머스 등산코스 정하기 풀이 예정

---
+++ 프로그래머스/Level3/등산코스정하기/등산코스정하기-1.py
@@ -0,0 +1,8 @@
+def solution(n, paths, gates, summits):
+    answer = []
+    return answer
+
+
+print(solution(6,
+               [[1, 2, 3], [2, 3, 5], [2, 4, 2], [2, 5, 4], [3, 4, 4], [4, 5, 3], [4, 6, 1], [5, 6, 1]],
+               [1, 3], [5])) # [5, 3]
