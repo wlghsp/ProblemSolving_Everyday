557cbb78db460e35f4ebe0374d513d2ab8d0b663
JIHO CHOI <wlghsp@gmail.com>
2025-11-04 09:25:59 +0900

    leetcode 120. Triangle 풀이 예정

---
+++ LeetCode/leetcode120_Triangle/Triangle-1.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        pass
+
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.minimumTotal([[2],[3,4],[6,5,7],[4,1,8,3]]))  # 11
+    print(sol.minimumTotal([[-10]]))  # -10
